e4666514604a488437238379ed26422ce0b72c66
norhanreda <norhanreda12345@gmail.com>
2022-10-07 13:46:36 +0200

    load the main component to test routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router.js'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router.js";
 
-const app=createApp(App);
+const app = createApp(App);
 
 app.use(router);
-app.mount('#app')
+app.mount("#app");
--- src/router.js
+++ src/router.js
@@ -1,21 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
-// import the components that you will use will routing
-
-
+import { createRouter, createWebHistory } from "vue-router";
+import TheMain from "./components/TheMain.vue";
 
 const router = createRouter({
-  history: createWebHistory,
+  history: createWebHistory(),
   routes: [
-    
-      {  path: '/',redirect:'',},
-      {  path: '/',component:'',},
-      
-      { path: '/r:notFound(.*)',component: null,}
-        
-      
-    
-
+    { path: "/", redirect: "/main" },
+    { path: "/main", component: TheMain },
+    //{ path: '/test', component: null },
+    //  { path: '', component: null },
   ],
 });
 
