eb8bf726c4de2542c1a3e9788c4475dd43e28666
norhanreda <norhanreda12345@gmail.com>
2022-10-08 18:03:31 +0200

    add  forget user name

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import { createApp } from "vue";
 import router from "./router.js";
 import store from "./store/index";
 import App from "./App.vue";
+import BaseButton from './components/BaseComponents/BaseButton.vue'
 
 const app = createApp(App);
 
 app.use(router);
 app.use(store);
+app.component('base-button',BaseButton);
 
 app.mount("#app");
