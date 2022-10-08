c662fb1b4ac5ed6838bddb6aa809ad2f62a7e3b4
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-10-08 11:39:08 +0200

    add vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+
 import router from "./router.js";
+import store from "./store/index";
+import App from "./App.vue";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(store);
+
 app.mount("#app");
