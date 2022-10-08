6376a0d77ed014477256edd814d392b921700351
MennaTalhHossamAlden <76497207+MennaTalhHossamAlden@users.noreply.github.com>
2022-10-08 17:40:33 +0200

    BaseButton

---
+++ src/components/BaseComponents/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button>{{ buttonText }}</button>
+</template>
+
+<script>
+    export default {
+        props: {
+            buttonText: String,
+        }
+    };
+</script>
+
+<style scoped>
+    button {
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 100px;   
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: background-color 0.2s;
+    }
+    button:disabled { 
+    cursor: not-allowed;
+    }
+</style>
