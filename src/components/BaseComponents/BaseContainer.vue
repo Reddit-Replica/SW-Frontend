90f4afe86299304d2cdf16b4f4c2d051bc85b265
medo emad <medo.emadd23@gmail.com>
2022-10-15 00:29:19 +0200

    profile + navbar

---
+++ src/components/BaseComponents/BaseContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="basecontainer">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+
+<style scoped>
+.basecontainer {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  padding: 20px 24px;
+}
+</style>
+
+
+
+
--- src/pages/users/UserPage.vue
+++ src/pages/users/UserPage.vue
@@ -1,17 +1,35 @@
 <template>
-  <h1>user page</h1>
+  <profile-nav></profile-nav>
+  <base-container>
+  <div class="profilebox">
+  <createpost-bar></createpost-bar>
+  <!-- <h1>user page</h1> -->
   <profile-card></profile-card>
+</div>
+</base-container>
 </template>
 
 <script>
 import ProfileCard from '../../components/UserComponents/ProfileCard.vue'
+import BaseContainer from '../../components/BaseComponents/BaseContainer.vue';
+import profileNav from '../../components/UserComponents/ProfileNav.vue';
+import createpostBar from '../../components/bars/CreatepostBar.vue';
 export default {
   components:{
-    ProfileCard
+    ProfileCard,
+    BaseContainer,
+    profileNav,
+    createpostBar
+
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.profilebox {
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  box-sizing: border-box;
+}
 </style>
