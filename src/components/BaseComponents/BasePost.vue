f3784ed4ef6670f49718084973e1de8d841e75d2
MennaTalhHossamAlden <76497207+MennaTalhHossamAlden@users.noreply.github.com>
2022-10-18 14:14:23 +0200

    post share submenu

--- src/components/BaseComponents/BasePost.vue
+++ src/components/BaseComponents/BasePost.vue
@@ -101,17 +101,25 @@
 							</svg>
 							{{ post.commentsCount }} Comments
 						</li>
-						<li>
+						<li id="awards">
 							<svg class="icon icon-gift">
 								<use xlink:href="../../../img/postServices.svg#icon-gift"></use>
 							</svg>
 							Awards
 						</li>
-						<li @click="showShareSubMenu">
-							<svg class="icon icon-share">
-								<use
-									xlink:href="../../../img/postServices.svg#icon-share"
-								></use>
+						<li @click="showShareSubMenu" id="share">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								class="bi bi-arrow-90deg-right"
+								viewBox="0 0 16 16"
+							>
+								<path
+									fill-rule="evenodd"
+									d="M14.854 4.854a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 4H3.5A2.5 2.5 0 0 0 1 6.5v8a.5.5 0 0 0 1 0v-8A1.5 1.5 0 0 1 3.5 5h9.793l-3.147 3.146a.5.5 0 0 0 .708.708l4-4z"
+								/>
 							</svg>
 							Share
 							<ul class="sub-menu" v-if="shareSubMenuDisplay">
@@ -148,8 +156,45 @@
 									</svg>
 									Crosspost
 								</li>
+								<li>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-code-square"
+										viewBox="0 0 16 16"
+									>
+										<path
+											d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+										/>
+										<path
+											d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z"
+										/>
+									</svg>
+									Embed
+								</li>
+								<li>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-chat-dots"
+										viewBox="0 0 16 16"
+									>
+										<path
+											d="M5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm4 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"
+										/>
+										<path
+											d="m2.165 15.803.02-.004c1.83-.363 2.948-.842 3.468-1.105A9.06 9.06 0 0 0 8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6a10.437 10.437 0 0 1-.524 2.318l-.003.011a10.722 10.722 0 0 1-.244.637c-.079.186.074.394.273.362a21.673 21.673 0 0 0 .693-.125zm.8-3.108a1 1 0 0 0-.287-.801C1.618 10.83 1 9.468 1 8c0-3.192 3.004-6 7-6s7 2.808 7 6c0 3.193-3.004 6-7 6a8.06 8.06 0 0 1-2.088-.272 1 1 0 0 0-.711.074c-.387.196-1.24.57-2.634.893a10.97 10.97 0 0 0 .398-2z"
+										/>
+									</svg>
+									Share&nbsp;To&nbsp;Chat
+								</li>
 							</ul>
 						</li>
+
 						<li id="post-direct-save">
 							<div v-if="!saved" @click="savePost">
 								<svg
@@ -199,6 +244,57 @@
 								/>
 							</svg>
 							<ul class="sub-menu" v-if="subMenuDisplay">
+								<li class="awards-item-in-sub-menu">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-gift"
+										viewBox="0 0 16 16"
+									>
+										<path
+											d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 14.5V7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A2.968 2.968 0 0 1 3 2.506V2.5zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43a.522.522 0 0 0 .023.07zM9 3h2.932a.56.56 0 0 0 .023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0V3zM1 4v2h6V4H1zm8 0v2h6V4H9zm5 3H9v8h4.5a.5.5 0 0 0 .5-.5V7zm-7 8V7H2v7.5a.5.5 0 0 0 .5.5H7z"
+										/>
+									</svg>
+									Awards
+								</li>
+								<li class="share-items-in-sub-menu">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-link-45deg"
+										viewBox="0 0 16 16"
+									>
+										<path
+											d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"
+										/>
+										<path
+											d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"
+										/>
+									</svg>
+									Copy&nbsp;Link
+								</li>
+								<li class="share-items-in-sub-menu">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-code-square"
+										viewBox="0 0 16 16"
+									>
+										<path
+											d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+										/>
+										<path
+											d="M6.854 4.646a.5.5 0 0 1 0 .708L4.207 8l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0zm2.292 0a.5.5 0 0 0 0 .708L11.793 8l-2.647 2.646a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708 0z"
+										/>
+									</svg>
+									Embed
+								</li>
 								<li v-if="!saved" @click="savePost" class="post-sub-save">
 									<svg
 										xmlns="http://www.w3.org/2000/svg"
@@ -552,6 +648,10 @@ a {
 .post-card .post-content .post-services .services .sub-menu li.post-sub-save {
 	display: none;
 }
+.post-content .post-services .services .sub-menu .share-items-in-sub-menu,
+.post-content .post-services .services .sub-menu .awards-item-in-sub-menu {
+	display: none;
+}
 @media (max-width: 1079px) {
 	.post-card .post-content .post-services .services .sub-menu li.post-sub-save {
 		display: flex;
@@ -567,6 +667,19 @@ a {
 
 	.post-services .vote-services {
 		display: inline !important;
+	}
+}
+@media (max-width: 519px) {
+	#share {
+		display: none;
+	}
+	.share-items-in-sub-menu {
+		display: flex !important;
+	}
+}
+@media (max-width: 495px) {
+	.awards-item-in-sub-menu {
+		display: flex;
 	}
 }
 </style>
