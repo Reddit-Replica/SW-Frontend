5fbd640f9c94b4bf752b9c71fb29b0ec257db616
norhanreda <norhanreda12345@gmail.com>
2022-10-19 22:41:08 +0200

    run

--- src/router.js
+++ src/router.js
@@ -3,21 +3,20 @@ import MainPage from './pages/users/MainPage.vue';
 import ForgetPassword from './components/forget/ForgetPassword.vue';
 import ForgetUsername from './components/forget/ForgetUsername.vue';
 import ForgetPasswordpage from './components/forget/ForgetPasswordpage.vue';
-import forgetUsernamepage from './components/forget/forgetUsernamepage.vue';
+import ForgetUsernamepage from './components/forget/ForgetUsernamepage.vue';
 import UserPage from './pages/users/UserPage.vue';
 import login from './pages/login/login.vue';
 import signupPage from './pages/sign-up-page/sign-up-page.vue';
 import NotFound from './pages/NotFound.vue';
 const router = createRouter({
-<<<<<<< Updated upstream
 	history: createWebHistory(),
 	routes: [
 		{ path: '/', redirect: '/main' },
 		{ path: '/main', component: MainPage },
 		{ path: '/forgetPassword', component: ForgetPassword },
 		{ path: '/forgetUsername', component: ForgetUsername },
-		{ path: '/ForgetPasswordpage', component: ForgetPasswordpage },
-		{ path: '/forgetUsernamepage', component: forgetUsernamepage },
+		{ path: '/forgetPasswordpage', component: ForgetPasswordpage },
+		{ path: '/forgetUsernamepage', component: ForgetUsernamepage },
 		{ path: '/login', component: login },
 		{ path: '/signup', component: signupPage },
 		{
@@ -41,34 +40,6 @@ const router = createRouter({
 		//  { path: '', component: null },
 		{ path: '/:notFound(.*)', component: NotFound },
 	],
-=======
-  history: createWebHistory(),
-  routes: [
-    { path: "/", redirect: "/main" },
-    { path: "/main", component: MainPage },
-    { path: '/forgetPassword', component: ForgetPassword },
-    { path: '/forgetUsername', component: ForgetUsername },
-    {
-      path: "/user/:userName",
-      name: "user",
-      component: UserPage,
-      children: [{ path: '/:jnj(.*)', component: UserPage }],
-      props: true
-    },//render user component
-    {
-      path: "/subreddit/:subredditName",
-      name: "subreddit",
-      component: MainPage,
-    },//render subreddit component
-    {
-      path: "/post/:postName",
-      name: "post",
-      component: MainPage,
-    },//render post component
-    //{ path: '/test', component: null },
-    //  { path: '', component: null },
-  ],
->>>>>>> Stashed changes
 });
 
 export default router;
