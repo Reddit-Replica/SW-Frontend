2ae5007d443f6c6812381dd2cad0b3eccbf3fc89
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-10-19 22:56:21 +0200

    add settings pages

--- src/router.js
+++ src/router.js
@@ -1,12 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import MainPage from './pages/users/MainPage.vue';
+import UserPage from './pages/users/UserPage.vue';
+
 import ForgetPassword from './components/forget/ForgetPassword.vue';
 import ForgetUsername from './components/forget/ForgetUsername.vue';
 import ForgetPasswordpage from './components/forget/ForgetPasswordpage.vue';
 import forgetUsernamepage from './components/forget/forgetUsernamepage.vue';
-import UserPage from './pages/users/UserPage.vue';
+
 import login from './pages/login/login.vue';
 import signupPage from './pages/sign-up-page/sign-up-page.vue';
+
+import TheSettings from './pages/settings/TheSettings.vue';
+import ProfileSettings from './pages/settings/ProfileSettings.vue';
+import PrivacySettings from './pages/settings/PrivacySettings.vue';
+import EmailsSettings from './pages/settings/EmailsSettings.vue';
+import FeedSettings from './pages/settings/FeedSettings.vue';
+import MessagingSettings from './pages/settings/MessagingSettings.vue';
+import AccountSettings from './pages/settings/AccountSettings.vue';
+
 import NotFound from './pages/NotFound.vue';
 const router = createRouter({
 	history: createWebHistory(),
@@ -39,6 +50,19 @@ const router = createRouter({
 		//{ path: '/test', component: null },
 		//  { path: '', component: null },
 		{ path: '/:notFound(.*)', component: NotFound },
+		{
+			path: '/settings',
+			name: 'settings',
+			component: TheSettings,
+			children: [
+				{ path: '/Account ', component: AccountSettings, alias: '/settings' },
+				{ path: '/Profile ', component: ProfileSettings },
+				{ path: '/privacy ', component: PrivacySettings },
+				{ path: '/feed ', component: FeedSettings },
+				{ path: '/emails ', component: EmailsSettings },
+				{ path: '/messaging ', component: MessagingSettings },
+			],
+		},
 	],
 });
 
