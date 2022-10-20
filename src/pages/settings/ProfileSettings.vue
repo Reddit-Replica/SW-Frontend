0c8aec3d6cf97f7d77c559e3e9d92e5424dc08ba
hodagamal <hoda.ismail00@eng-st.cu.edu.eg>
2022-10-20 17:55:29 +0200

    First step in profile settings page ( fonts styling )

--- src/pages/settings/ProfileSettings.vue
+++ src/pages/settings/ProfileSettings.vue
@@ -1,9 +1,145 @@
 <template>
-	<div>profile settings</div>
+	<div class="settings-page">
+		<div class="settings-block">
+			<h2 class="h2-main-title">Customize profile</h2>
+			<h3 class="h3-main-title">PROFILE INFORMATION</h3>
+
+			<div>
+				<div>
+					<h3 class="h3-title">Display name (optional)</h3>
+					<p class="p-title-description">
+						Set a display name. This does not change your username.
+					</p>
+				</div>
+				<div>
+					<input
+						placeholder="Display name (optional)"
+						maxlength="30"
+						type="text"
+						name="dispalyname"
+						id="dispalyname"
+						class="input-field"
+					/>
+					<div class="p-comment">30 Characters remaining</div>
+				</div>
+			</div>
+
+			<div>
+				<div>
+					<h3 class="h3-title">About (optional)</h3>
+					<p class="p-title-description">
+						A brief description of yourself shown on your profile.
+					</p>
+				</div>
+				<div>
+					<textarea
+						placeholder="About (optional)"
+						maxlength="200"
+						name="about"
+						id="about"
+						rows="4"
+						class="text-area-field"
+					></textarea>
+					<div class="p-comment">200 Characters remaining</div>
+				</div>
+			</div>
+
+			<div>
+				<div>
+					<h3 class="h3-title">Social links (5 max)</h3>
+					<p class="p-title-description">
+						People who visit your profile will see your social links.
+					</p>
+				</div>
+				<div>
+					<div>
+						<div>
+							<!-- icon -->
+							Add social link
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<h3 class="h3-main-title">IMAGES</h3>
+
+			<div>
+				<div>
+					<h3 class="h3-title">Profile picture and banner image</h3>
+					<p class="p-title-description">Images must be .png or .jpg format</p>
+				</div>
+				<div>
+					<div>
+						<div>
+							<label for=""></label>
+						</div>
+						<div>
+							<label for="">
+								<!-- <div>icon</div> -->
+								<div></div>
+								<div>
+									<input type="file" name="" accept="image/x-png,image/jpeg" />
+								</div>
+							</label>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<h3 class="h3-main-title">PROFILE CATEGORY</h3>
+
+			<div>
+				<div>
+					<div>
+						<label for="">
+							<h3 class="h3-title">NSFW</h3>
+						</label>
+					</div>
+					<p class="p-title-description">
+						This content is NSFW (may contain nudity, pornography, profanity or
+						inappropriate content for those under 18)
+					</p>
+				</div>
+				<div>
+					<div>
+						<button></button>
+					</div>
+				</div>
+			</div>
+
+			<h3 class="h3-main-title">ADVANCED</h3>
+
+			<div>
+				<div>
+					<div>
+						<label for="">
+							<h3 class="h3-title">Allow people to follow you</h3>
+						</label>
+					</div>
+					<p class="p-title-description">
+						Followers will be notified about posts you make to your profile and
+						see them in their home feed.
+					</p>
+				</div>
+				<div>
+					<div>
+						<button></button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script>
-export default {};
-</script>
-
-<style></style>
+<style scoped>
+.settings-page {
+	max-width: 1200px;
+	margin: 0 auto;
+	padding: 0 16px;
+	display: flex;
+}
+.settings-block {
+	max-width: 688px;
+	flex: 1 1 auto;
+}
+</style>
--- src/pages/settings/TheSettings.vue
+++ src/pages/settings/TheSettings.vue
@@ -1,9 +1,72 @@
 <template>
 	<div>the settings</div>
+	<div><profile-settings></profile-settings></div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+.h2-main-title {
+	font-size: 20px;
+	font-weight: 500;
+	line-height: 24px;
+	color: black;
+	padding: 40px 0;
+}
+.h3-main-title {
+	font-size: 10px;
+	font-weight: 700;
+	letter-spacing: 0.5px;
+	line-height: 12px;
+	text-transform: uppercase;
+	border-bottom: 1px solid lightgray;
+	color: gray;
+	margin-bottom: 32px;
+	padding-bottom: 6px;
+}
+.h3-title {
+	font-size: 16px;
+	font-weight: 500;
+	line-height: 20px;
+	color: black;
+	display: flex;
+	margin-bottom: 4px;
+}
+.p-title-description {
+	font-weight: 400;
+	color: gray;
+	font-size: 12px;
+	line-height: 16px;
+}
+.input-field,
+.text-area-field {
+	background-color: white;
+	border: 1px solid lightgray;
+	color: black;
+	box-sizing: border-box;
+	border-radius: 4px;
+	width: 100%;
+	font-size: 14px;
+	font-weight: 400;
+	line-height: 21px;
+}
+.input-field {
+	height: 48px;
+	margin-bottom: 8px;
+	padding: 12px 24px 4px 12px;
+}
+.text-area-field {
+	resize: both;
+	margin-bottom: 0px;
+	padding: 8px;
+}
+.p-comment {
+	color: gray;
+	padding-top: 5px;
+	font-weight: 400;
+	font-size: 12px;
+	line-height: 16px;
+}
+</style>
