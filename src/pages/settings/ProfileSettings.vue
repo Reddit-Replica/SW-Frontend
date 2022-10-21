8519f0d41e5907b504cfc2da85723112426f72db
hodagamal <hoda.ismail00@eng-st.cu.edu.eg>
2022-10-21 10:16:54 +0200

    Update profile settings page styling

--- src/pages/settings/ProfileSettings.vue
+++ src/pages/settings/ProfileSettings.vue
@@ -4,14 +4,14 @@
 			<h2 class="h2-main-title">Customize profile</h2>
 			<h3 class="h3-main-title">PROFILE INFORMATION</h3>
 
-			<div>
-				<div>
+			<div class="box box-flex-column">
+				<div class="box-1">
 					<h3 class="h3-title">Display name (optional)</h3>
 					<p class="p-title-description">
 						Set a display name. This does not change your username.
 					</p>
 				</div>
-				<div>
+				<div class="box-2">
 					<input
 						placeholder="Display name (optional)"
 						maxlength="30"
@@ -24,14 +24,14 @@
 				</div>
 			</div>
 
-			<div>
-				<div>
+			<div class="box box-flex-column">
+				<div class="box-1">
 					<h3 class="h3-title">About (optional)</h3>
 					<p class="p-title-description">
 						A brief description of yourself shown on your profile.
 					</p>
 				</div>
-				<div>
+				<div class="box-2">
 					<textarea
 						placeholder="About (optional)"
 						maxlength="200"
@@ -44,17 +44,28 @@
 				</div>
 			</div>
 
-			<div>
-				<div>
+			<div class="box box-flex-column">
+				<div class="box-1">
 					<h3 class="h3-title">Social links (5 max)</h3>
 					<p class="p-title-description">
 						People who visit your profile will see your social links.
 					</p>
 				</div>
-				<div>
-					<div>
-						<div>
-							<!-- icon -->
+				<div class="box-2">
+					<div class="box-social-links">
+						<div class="box-icon-social-links">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								class="bi bi-plus"
+								viewBox="0 0 16 16"
+							>
+								<path
+									d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+								/>
+							</svg>
 							Add social link
 						</div>
 					</div>
@@ -63,22 +74,75 @@
 
 			<h3 class="h3-main-title">IMAGES</h3>
 
-			<div>
-				<div>
+			<div class="box box-flex-column">
+				<div class="box-1">
 					<h3 class="h3-title">Profile picture and banner image</h3>
 					<p class="p-title-description">Images must be .png or .jpg format</p>
 				</div>
-				<div>
-					<div>
-						<div>
-							<label for=""></label>
+				<div class="box-2">
+					<div class="box-images">
+						<div class="box-image-1">
+							<label class="box-image-label" for="profile-picture">
+								<div class="icon-plus-circle">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-plus-circle"
+										viewBox="0 0 16 16"
+									>
+										<path
+											d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+										/>
+										<path
+											d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+										/>
+									</svg>
+								</div>
+								<div class="drag-drop-title">
+									Drag and Drop or Upload
+									<span class="bold">Profile</span> Image
+								</div>
+								<div class="initial-image-input">
+									<input
+										class="image-input"
+										type="file"
+										name="banner"
+										accept="image/x-png,image/jpeg"
+									/>
+								</div>
+							</label>
 						</div>
-						<div>
-							<label for="">
-								<!-- <div>icon</div> -->
-								<div></div>
-								<div>
-									<input type="file" name="" accept="image/x-png,image/jpeg" />
+						<div class="box-image-2">
+							<label class="box-image-label" for="banner">
+								<div class="icon-plus-circle">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										width="16"
+										height="16"
+										fill="currentColor"
+										class="bi bi-plus-circle"
+										viewBox="0 0 16 16"
+									>
+										<path
+											d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+										/>
+										<path
+											d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+										/>
+									</svg>
+								</div>
+								<div class="drag-drop-title">
+									Drag and Drop or Upload <span class="bold">Banner</span> Image
+								</div>
+								<div class="initial-image-input">
+									<input
+										class="image-input"
+										type="file"
+										name="banner"
+										accept="image/x-png,image/jpeg"
+									/>
 								</div>
 							</label>
 						</div>
@@ -88,10 +152,10 @@
 
 			<h3 class="h3-main-title">PROFILE CATEGORY</h3>
 
-			<div>
-				<div>
+			<div class="box">
+				<div class="box-1">
 					<div>
-						<label for="">
+						<label for="nsfw">
 							<h3 class="h3-title">NSFW</h3>
 						</label>
 					</div>
@@ -100,19 +164,19 @@
 						inappropriate content for those under 18)
 					</p>
 				</div>
-				<div>
+				<div class="box-switch-button">
 					<div>
-						<button></button>
+						<switch-button></switch-button>
 					</div>
 				</div>
 			</div>
 
 			<h3 class="h3-main-title">ADVANCED</h3>
 
-			<div>
-				<div>
+			<div class="box">
+				<div class="box-1">
 					<div>
-						<label for="">
+						<label for="allow">
 							<h3 class="h3-title">Allow people to follow you</h3>
 						</label>
 					</div>
@@ -121,9 +185,9 @@
 						see them in their home feed.
 					</p>
 				</div>
-				<div>
+				<div class="box-switch-button">
 					<div>
-						<button></button>
+						<switch-button></switch-button>
 					</div>
 				</div>
 			</div>
@@ -131,15 +195,107 @@
 	</div>
 </template>
 
+<script>
+import SwitchButton from '../../components/SwitchButton.vue';
+export default {
+	components: {
+		SwitchButton,
+	},
+};
+</script>
+
 <style scoped>
-.settings-page {
-	max-width: 1200px;
-	margin: 0 auto;
-	padding: 0 16px;
+.box-social-links {
+	width: 1200px;
 	display: flex;
+	flex-wrap: wrap;
+	row-gap: 8px;
 }
-.settings-block {
-	max-width: 688px;
-	flex: 1 1 auto;
+.box-icon-social-links {
+	font-size: 12px;
+	font-weight: 700;
+	line-height: 16px;
+	align-items: center;
+	background-color: lightgray;
+	border-radius: 100px;
+	color: black;
+	cursor: pointer;
+	display: flex;
+	height: 20px;
+	margin-right: 8px;
+	padding: 10px 12px;
+	white-space: nowrap;
+	box-sizing: content-box;
 }
+.bi-plus {
+	margin-right: 8px;
+}
+.box-images {
+	display: flex;
+	height: 120px;
+}
+.box-image-1 {
+	height: 100%;
+	margin: 0;
+	width: 120px;
+	margin: 0 12px 0 0;
+}
+.box-image-2 {
+	height: 100%;
+	margin: 0;
+	width: 412px;
+}
+.box-image-label {
+	border: 1px dashed #d7d7d7;
+	border-radius: 8px;
+	box-sizing: border-box;
+	padding: 4px;
+	cursor: pointer;
+	align-items: center;
+	background-color: white;
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	justify-content: center;
+	text-align: center;
+	width: 100%;
+}
+.icon-plus-circle {
+	margin-top: -8px;
+	margin-bottom: 4px;
+	cursor: pointer;
+}
+.bi-plus-circle {
+	fill: var(--color-blue-2);
+	height: 36px;
+	stroke: var(--color-blue-2);
+	width: 36px;
+	border-radius: 50%;
+}
+.drag-drop-title {
+	color: gray;
+	font-size: 11px;
+}
+.bold {
+	font-weight: 700;
+}
+.initial-image-input {
+	display: none;
+}
+.image-input {
+	font-size: 14px;
+	font-weight: 400;
+	line-height: 21px;
+}
+/* .image-input[type='file' i] {
+	appearance: none;
+	background-color: initial;
+	cursor: default;
+	align-items: baseline;
+	color: inherit;
+	text-overflow: ellipsis;
+	white-space: pre;
+	text-align: start !important;
+	overflow: hidden !important;
+} */
 </style>
--- src/pages/settings/TheSettings.vue
+++ src/pages/settings/TheSettings.vue
@@ -3,16 +3,32 @@
 	<div><profile-settings></profile-settings></div>
 </template>
 
-<script>
-export default {};
-</script>
+<!-- <script>
+import SwitchButton from '../../components/SwitchButton.vue';
+export default {
+	components: {
+		SwitchButton,
+	},
+};
+</script> -->
 
 <style>
+.settings-page {
+	max-width: 1200px;
+	margin: 0 auto;
+	padding: 0 16px;
+	display: flex;
+	background-color: var(--color-white-1);
+}
+.settings-block {
+	max-width: 688px;
+	flex: 1 1 auto;
+}
 .h2-main-title {
 	font-size: 20px;
 	font-weight: 500;
 	line-height: 24px;
-	color: black;
+	color: var(--color-dark-1);
 	padding: 40px 0;
 }
 .h3-main-title {
@@ -21,8 +37,8 @@ export default {};
 	letter-spacing: 0.5px;
 	line-height: 12px;
 	text-transform: uppercase;
-	border-bottom: 1px solid lightgray;
-	color: gray;
+	border-bottom: 1px solid var(--color-grey-light-2);
+	color: var(--color-grey-dark-2);
 	margin-bottom: 32px;
 	padding-bottom: 6px;
 }
@@ -30,21 +46,21 @@ export default {};
 	font-size: 16px;
 	font-weight: 500;
 	line-height: 20px;
-	color: black;
+	color: var(--color-dark-1);
 	display: flex;
 	margin-bottom: 4px;
 }
 .p-title-description {
 	font-weight: 400;
-	color: gray;
+	color: var(--color-grey-dark-2);
 	font-size: 12px;
 	line-height: 16px;
 }
 .input-field,
 .text-area-field {
-	background-color: white;
-	border: 1px solid lightgray;
-	color: black;
+	background-color: var(--color-white-1);
+	border: 1px solid var(--color-grey-light-2);
+	color: var(--color-grey-dark-1);
 	box-sizing: border-box;
 	border-radius: 4px;
 	width: 100%;
@@ -63,10 +79,37 @@ export default {};
 	padding: 8px;
 }
 .p-comment {
-	color: gray;
+	color: var(--color-grey-dark-2);
 	padding-top: 5px;
 	font-weight: 400;
 	font-size: 12px;
 	line-height: 16px;
+}
+.box {
+	display: flex;
+	flex-flow: row wrap;
+	margin-bottom: 32px;
+}
+.box-flex-column {
+	flex-direction: column;
+}
+.box-1 {
+	display: flex;
+	flex-direction: column;
+	margin-right: 8px;
+	max-width: 80%;
+}
+.box-2 {
+	align-items: flex-start;
+	margin-top: 12px;
+	flex-direction: column;
+	display: flex;
+	justify-content: flex-end;
+}
+.box-switch-button {
+	align-items: center;
+	display: flex;
+	justify-content: flex-end;
+	flex-grow: 1;
 }
 </style>
