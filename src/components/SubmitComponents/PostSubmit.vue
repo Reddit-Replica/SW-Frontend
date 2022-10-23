cb785d588e37c9c2b622d7a71c4e259302772634
norhanreda <norhanreda12345@gmail.com>
2022-10-23 20:23:27 +0200

    fix a bug

--- src/components/SubmitComponents/PostSubmit.vue
+++ src/components/SubmitComponents/PostSubmit.vue
@@ -2,7 +2,6 @@
 	<div>
 		<div class="bold-icon"></div>
 	</div>
-	<textarea></textarea>
 </template>
 
 <script>
@@ -11,13 +10,5 @@ export default {};
 
 <style scoped>
 .bold-icon {
-	content: '';
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	border-radius: 9999px;
-	opacity: 0;
 }
 </style>
