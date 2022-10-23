4e93c362940347e55093484ac7b36598558c358d
norhanreda <norhanreda12345@gmail.com>
2022-10-23 18:42:53 +0200

    adding things to emails

--- src/pages/settings/EmailsSettings.vue
+++ src/pages/settings/EmailsSettings.vue
@@ -1,28 +1,129 @@
 <template>
 	<div class="settings-page">
-		<div class="in-box">
-			<h2>Manage Emails</h2>
-			<h3>messages</h3>
+		<div class="settings-block">
 			<div>
-				<h3>Inbox messages</h3>
-				<switch-button> </switch-button>
-				<h3>Chat requests</h3>
-				<switch-button> </switch-button>
+				<h2 class="h2-main-title">Manage Emails</h2>
+				<h3 class="h3-main-title">messages</h3>
+
+				<div class="box-con">
+					<h3 class="h3-title">Inbox messages</h3>
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<div class="box-con">
+					<h3 class="h3-title">Chat requests</h3>
+
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+			</div>
+
+			<div>
+				<h3 class="h3-main-title">ACTIVITY</h3>
+
+				<div class="box-con">
+					<h3 class="h3-title">New user welcome</h3>
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<div class="box-con">
+					<h3 class="h3-title">Comments on your posts</h3>
+
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<div class="box-con">
+					<h3 class="h3-title">Replies to your comments</h3>
+
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<div class="box-con">
+					<h3 class="h3-title">Upvotes on your posts</h3>
+
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<div class="box-con">
+					<h3 class="h3-title">Upvotes on your comments</h3>
+
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<div class="box-con">
+					<h3 class="h3-title">Username mentions</h3>
+
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<div class="box-con">
+					<h3 class="h3-title">New followers</h3>
+
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<h3 class="h3-main-title">NEWSLETTERS</h3>
+
+				<div class="box-con">
+					<h3 class="h3-title">Daily Digest</h3>
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<div class="box-con">
+					<h3 class="h3-title">Weekly Recap</h3>
+
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+				<div class="box-con">
+					<h3 class="h3-title">Community Discovery</h3>
+
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
+			</div>
+			<div>
+				<h3 class="h3-main-title"></h3>
+
+				<div class="box-con">
+					<h3 class="h3-title">Unsubscribe from all emails</h3>
+					<div class="box-switch-button">
+						<switch-button></switch-button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import SwitchButton from '../../components/SwitchButton.vue';
+export default {
+	components: {
+		SwitchButton,
+	},
+};
 </script>
 
 <style scoped>
-div {
-	display: block;
+body {
+	font-family: 'IBM Plex Sans', sans-serif;
 }
-.big-cont {
-	background-color: white;
+.box-con {
 	display: flex;
+	flex-flow: row wrap;
+	margin-bottom: 32px;
 }
 </style>
