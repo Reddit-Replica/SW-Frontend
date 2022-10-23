86f388315466ac35712103b80b19c202e4382b11
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-10-23 21:25:48 +0200

    add logout to sub menu

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -217,6 +217,26 @@
 						Dark Mode
 						<switch-button></switch-button>
 					</li>
+					<li class="setting-choice-with-icon">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							width="16"
+							height="16"
+							fill="currentColor"
+							class="bi bi-box-arrow-in-right"
+							viewBox="0 0 16 16"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"
+							/>
+							<path
+								fill-rule="evenodd"
+								d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"
+							/>
+						</svg>
+						Logout
+					</li>
 				</ul>
 			</div>
 		</nav>
@@ -543,8 +563,13 @@ export default {
 	justify-content: space-between;
 	align-items: center;
 }
-.setting-choice:hover {
+.setting-choice:hover,
+.setting-choice-with-icon:hover {
 	background-color: var(--color-grey-light-2);
+}
+.setting-choice-with-icon {
+	color: var(--color-dark-1);
+	padding: 1rem !important;
 }
 /* 1200px */
 @media only screen and (max-width: 75em) {
