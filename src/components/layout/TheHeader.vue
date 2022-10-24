4e7736a8f2cd25234db6caa5e75698ac323c7fbe
MennaTalhHossamAlden <76497207+MennaTalhHossamAlden@users.noreply.github.com>
2022-10-24 17:35:40 +0200

    header dynamically changing with routes

--- src/components/layout/TheHeader.vue
+++ src/components/layout/TheHeader.vue
@@ -5,7 +5,7 @@
 			<img src="../../../img/reddit.png" alt="reddit" class="header-reddit" />
 		</div>
 		<div class="header-home" @click="showHomeSubMenu()">
-			<div class="header-icon-home">
+			<div class="header-icon-home" v-if="$route.path == '/main'">
 				<svg
 					xmlns="http://www.w3.org/2000/svg"
 					width="16"
@@ -19,6 +19,16 @@
 					/>
 				</svg>
 				<span>Home</span>
+			</div>
+			<div
+				@click="goToSettings()"
+				class="color-black"
+				v-else-if="$route.path == '/settings' || '/settings/'"
+			>
+				<div>
+					<img src="../../../img/user-image.jpg" alt="img" class="users-img" />
+					User Settings
+				</div>
 			</div>
 			<svg class="header-user-nav-icon header-down-arrow">
 				<use xlink:href="../../../img/sprite.svg#icon-chevron-small-down" />
@@ -772,6 +782,9 @@ export default {
 	align-items: center;
 	justify-content: space-between;
 	font-size: 1.2rem;
+}
+.color-black {
+	color: black;
 }
 .home {
 	fill: var(--color-dark-1) !important;
