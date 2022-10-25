04d10ff6a78039b144770059708f8ef4f86762ef
norhanreda <norhanreda12345@gmail.com>
2022-10-25 14:06:53 +0200

    adding took tip to all icons

--- src/components/SubmitComponents/PostSubmit.vue
+++ src/components/SubmitComponents/PostSubmit.vue
@@ -1,102 +1,144 @@
 <template>
 	<div class="big-box">
 		<div class="icons-box">
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-bold " />
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-italic" />
-				</div>
-			</button>
-			<button class="icons">
-				<div>
-					<svg
-						class="svg-inline--fa fa-link icon"
-						data-v-d316172c=""
-						aria-hidden="true"
-						focusable="false"
-						data-prefix="fas"
-						data-icon="link"
-						role="img"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 640 512"
-						data-fa-i2svg=""
-					>
-						<path
-							fill="currentColor"
-							d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"
-						></path>
-					</svg>
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-strikethrough" />
-				</div>
-			</button>
-
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-code" />
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-superscript" />
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-exclamation" />
-				</div>
-			</button>
-
+			<div class="tool-tip">
+				<span class="tool-tip-text">Bold</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-bold " />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text">Italics</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-italic" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text">Link</span>
+				<button class="icons">
+					<div>
+						<svg
+							class="svg-inline--fa fa-link icon"
+							data-v-d316172c=""
+							aria-hidden="true"
+							focusable="false"
+							data-prefix="fas"
+							data-icon="link"
+							role="img"
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 640 512"
+							data-fa-i2svg=""
+						>
+							<path
+								fill="currentColor"
+								d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"
+							></path>
+						</svg>
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Strikethrough</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-strikethrough" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Inline Code</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-code" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Superscript</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-superscript" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text">Spoiler</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-exclamation" />
+					</div>
+				</button>
+			</div>
 			<div class="space-in"></div>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-heading" />
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-list-ul" />
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-list-ol" />
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-quote-right" />
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-regular fa-file-code" />
-				</div>
-			</button>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Heading</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-heading" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Bulleted List</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-list-ul" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Numbered List</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-list-ol" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Quote Block</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-quote-right" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Code Block</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-regular fa-file-code" />
+					</div>
+				</button>
+			</div>
 			<div class="space-in"></div>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-solid fa-table" />
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-regular fa-image" />
-				</div>
-			</button>
-			<button class="icons">
-				<div class="icon">
-					<font-awesome-icon icon="fa-brands fa-youtube" />
-				</div>
-			</button>
-
+			<div class="tool-tip">
+				<span class="tool-tip-text">Table</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-solid fa-table" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Add an image</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-regular fa-image" />
+					</div>
+				</button>
+			</div>
+			<div class="tool-tip">
+				<span class="tool-tip-text strike">Add a video</span>
+				<button class="icons">
+					<div class="icon">
+						<font-awesome-icon icon="fa-brands fa-youtube" />
+					</div>
+				</button>
+			</div>
 			<button class="mark-down">Markdown Mode</button>
 		</div>
 		<div class="text-box">
@@ -204,6 +246,44 @@ div {
 	height: 14px;
 	margin: 0 6px;
 	width: 1px;
+}
+.tool-tip {
+	position: relative;
+	cursor: pointer;
+}
+.tool-tip-text {
+	text-align: center;
+	width: 60px;
+	height: 25px;
+	position: absolute;
+	color: white;
+	background-color: black;
+	border: 2px solid black;
+	font-family: 'Noto Sans', Arial, sans-serif;
+	font-size: 12px;
+	font-weight: 400;
+	border-radius: 5px;
+	visibility: hidden;
+	padding: 2px 2px 20px 2px;
+	transform: translateX(-20%);
+	opacity: 0;
+}
+.strike {
+	width: 100px;
+}
+.tool-tip-text::before {
+	content: '';
+	position: absolute;
+	left: 50%;
+	top: 100%;
+	border: 8px solid;
+	transform: translateX(-50%);
+	border-color: #000 #0000 #0000 #0000;
+}
+.tool-tip:hover .tool-tip-text {
+	top: -40px;
+	visibility: visible;
+	opacity: 1;
 }
 .mark-down {
 	border-radius: 9999px;
