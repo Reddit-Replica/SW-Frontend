a6201cc976b33e958e7da0105ae9c1e34b50b980
medo emad <medo.emadd23@gmail.com>
2022-11-02 15:41:34 +0200

    adjust_some_user_styling

---
+++ src/components/UserComponents/BaseUserComponents/SociallinksBlock.vue
@@ -0,0 +1,42 @@
+<template>
+	<ul class="social-link" id="social-media-links">
+		<a href="">
+			<sociallink-item
+				imgSrc="https://www.redditstatic.com/desktop2x/img/social-links/custom.png"
+				text="Custom URL"
+			></sociallink-item>
+		</a>
+		<sociallink-item
+			imgSrc="https://www.redditstatic.com/desktop2x/img/social-links/custom.png"
+			text="Custom URL"
+		></sociallink-item>
+		<li>
+			<button class="add-social-link" @click="openSocialLinkDialog">
+				<span><i class="fa-solid fa-plus" /></span>Add social link
+			</button>
+		</li>
+	</ul>
+</template>
+
+<script>
+import SociallinkItem from './SociallinkItem.vue';
+export default {
+	components: {
+		SociallinkItem,
+	},
+};
+</script>
+
+<style scoped>
+ul {
+	list-style: none;
+	border-bottom: none;
+}
+.social-link {
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	flex-wrap: wrap;
+	row-gap: 8px;
+}
+</style>
