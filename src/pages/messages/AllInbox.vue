71214b9710e498291948d353b5cfbe5c0a0bc0e9
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-11-03 15:03:57 +0200

    add unread messages and user mentions

--- src/pages/messages/AllInbox.vue
+++ src/pages/messages/AllInbox.vue
@@ -1,11 +1,15 @@
 <template>
 	<div>
-		<allinbox-component
-			v-for="message in messages"
-			:key="message"
-			:message="message"
-		></allinbox-component>
-		<!-- <button @click="load()"></button> -->
+		<div>
+			<allinbox-component
+				v-for="message in composeMessages"
+				:key="message"
+				:message="message"
+			></allinbox-component>
+		</div>
+		<div class="no-messages" v-if="noMessages">
+			there doesn't seem to be anything here
+		</div>
 	</div>
 </template>
 
@@ -15,29 +19,29 @@ export default {
 	components: {
 		AllinboxComponent,
 	},
+	beforeMount() {
+		document.title = 'messages: inbox';
+		this.loadComposeMessages();
+	},
 	data() {
 		return {
-			// messages: [],
+			noMessages: false,
 		};
 	},
-	created() {
-		document.title = 'messages: inbox';
-		this.loadAllMessages();
-	},
-	// watch: {
-	// 	messages() {
-	// 		console.log(this.messages);
-	// 	},
-	// },
 	computed: {
-		messages() {
-			return this.$store.getters['messages/allMessages'];
+		composeMessages() {
+			return this.$store.getters['messages/composeMessages'];
+		},
+	},
+	watch: {
+		composeMessages() {
+			if (this.composeMessages.length == 0) this.noMessages = true;
 		},
 	},
 	methods: {
-		async loadAllMessages() {
+		async loadComposeMessages() {
 			try {
-				await this.$store.dispatch('messages/loadAllMessages', {
+				await this.$store.dispatch('messages/loadComposeMessages', {
 					baseurl: this.$baseurl,
 				});
 			} catch (error) {
--- src/store/modules/messages/mutations.js
+++ src/store/modules/messages/mutations.js
@@ -2,4 +2,10 @@ export default {
 	setAllMessages(state, payload) {
 		state.allMessages = payload;
 	},
+	setUnreadMessages(state, payload) {
+		state.unreadMessages = payload;
+	},
+	setUserMentions(state, payload) {
+		state.userMentions = payload;
+	},
 };
