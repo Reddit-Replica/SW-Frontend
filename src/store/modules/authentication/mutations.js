36997078f4628dee17f370be674212435c1a6250
norhanreda <Norhan.ahmed01@eng-st.cu.edu.eg>
2022-11-03 11:54:52 +0200

    make npm run all run server not auth

--- src/store/modules/authentication/mutations.js
+++ src/store/modules/authentication/mutations.js
@@ -1,10 +1,10 @@
 export default {
 	// setUser(state, payload) {
-	//   state.token = payload.token;
-	//   state.userId = payload.userId;
-	//   state.didAutoLogout = false;
+	// 	state.token = payload.token;
+	// 	state.userId = payload.userId;
+	// 	state.didAutoLogout = false;
 	// },
 	// setAutoLogout(state) {
-	//   state.didAutoLogout = true;
-	// }
+	// 	state.didAutoLogout = true;
+	// },
 };
