f4e3fd6eb57ef6beeae8f4d5c18ebd4ef553beca
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-11-07 21:22:07 +0200

    update message form unit tests

--- tests/messages/MessageForm.spec.ts
+++ tests/messages/MessageForm.spec.ts
@@ -2,9 +2,15 @@ import { mount } from '@vue/test-utils';
 import MessageForm from '../../src/pages/messages/MessageForm.vue';
 import { describe, it, expect } from 'vitest';
 describe('MessageForm.vue', () => {
+	//--------------------------------------------------------
+	//                     Rendering
+	//--------------------------------------------------------
 	it('should render', () => {
 		const wrapper = mount(MessageForm);
 	});
+	//--------------------------------------------------------
+	//        Check the error in submitting empty fields
+	//--------------------------------------------------------
 	it('test error in empty fields', async () => {
 		const wrapper = mount(MessageForm, {
 			data() {
@@ -48,6 +54,63 @@ describe('MessageForm.vue', () => {
 				console.log('Promise Rejected');
 			});
 	});
+
+	//--------------------------------------------------------
+	//        Check text of button
+	//--------------------------------------------------------
+	it('Contains formatting button', () => {
+		const wrapper = mount(MessageForm);
+		const formattingBtn = wrapper.find('#formatting-button');
+		expect(
+			formattingBtn.text() == 'formatting help' ||
+				formattingBtn.text() == 'hide help'
+		).toBe(true);
+	});
+
+	it('Contains content policy link', () => {
+		const wrapper = mount(MessageForm);
+		const contentPolicyBtn = wrapper.find('#content-policy-link');
+		expect(contentPolicyBtn.text() == 'content policy').toBe(true);
+	});
+
+	//--------------------------------------------------------
+	//        Check clicking on formatting button
+	//--------------------------------------------------------
+	it('test clicking on formatting button', async () => {
+		const wrapper = mount(MessageForm);
+		const formattingBtn = wrapper.find('#formatting-button');
+		formattingBtn
+			.trigger('click')
+			.then(() => {
+				expect(wrapper.text()).contain(
+					'reddit uses a slightly-customized version of Markdown for formatting. See below for some basics, or check the commenting wiki page for more detailed help and solutions to common issues.'
+				);
+			})
+			.catch(function () {
+				console.log('Promise Rejected');
+			});
+	});
+		//--------------------------------------------------------
+	//        Check clicking on hide button
+	//--------------------------------------------------------
+	it('test clicking on hide button', async () => {
+		const wrapper = mount(MessageForm);
+		const formattingBtn = wrapper.find('#formatting-button');
+		formattingBtn
+			.trigger('click')
+			.then(() => {
+				expect(wrapper.text()).not.contain(
+					'reddit uses a slightly-customized version of Markdown for formatting. See below for some basics, or check the commenting wiki page for more detailed help and solutions to common issues.'
+				);
+			})
+			.catch(function () {
+				console.log('Promise Rejected');
+			});
+	});
+
+	//--------------------------------------------------------
+	//        Check success submitting
+	//--------------------------------------------------------
 	it('test successfully recieved', async () => {
 		const wrapper = mount(MessageForm, {
 			data() {
