7db23cfa0a136625fa103bd70b039196b40ff3a6
norhanreda <Norhan.ahmed01@eng-st.cu.edu.eg>
2022-11-07 16:02:12 +0200

    connect forgetpassword to backend

--- src/store/modules/authentication/actions.js
+++ src/store/modules/authentication/actions.js
@@ -4,21 +4,27 @@ export default {
 			username: payload.username,
 			email: payload.email,
 		};
-		const baseurl = payload.baseurl;
-
-		const response = await fetch(baseurl + '/login/forget-password', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(userInfo),
-		});
+		//const baseurl = payload.baseurl;
+		//baseurl + '/login/forget-password'
+		const response = await fetch(
+			'http://localhost:3000/login/forget-password',
+			{
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(userInfo),
+			}
+		);
 
 		const responseData = await response.json();
+		console.log(responseData);
 
 		if (!response.ok) {
-			const error = new Error(
-				responseData.message || 'Failed to send request.'
-			);
+			const error = new Error(responseData.error);
 			throw error;
 		}
+		context.commit('setUser', {
+			userName: userInfo.username,
+			accessToken: responseData.split(' ')[1],
+		});
 	},
 };
