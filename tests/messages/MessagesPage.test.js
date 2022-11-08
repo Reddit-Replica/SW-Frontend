3c4c7930d47396a50ff1b37eba28baa195085260
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-11-09 00:20:12 +0200

    messages page nit testing

---
+++ tests/messages/MessagesPage.test.js
@@ -0,0 +1,36 @@
+import {mount} from '@vue/test-utils';
+import {shallowMount} from '@vue/test-utils';
+import MessagesInbox from '../../src/pages/messages/MessagesInbox.vue';
+import Vuex from 'vuex';
+import store from '../../src/store/index.js';
+
+import {describe, it, expect} from 'vitest';
+describe ('MessagesInbox', () => {
+  const message = {
+    id: 2,
+    text: 'hello asmaa',
+    type: 'Messages',
+    subredditName: 'subredditName',
+    postTitle: 'postTitle',
+    senderUsername: '/u/hoda_gamal',
+    receiverUsername: '/u/asmaaadel0',
+    subject: 'hi',
+    sendAt: '2019-08-24T14:15:22Z',
+    isReply: false,
+    isRead: true,
+  };
+  it ('should renders if MessagesInbox content is correct', () => {
+    const wrapper = shallowMount (MessagesInbox, {
+      props: {
+        message,
+      },
+      global: {
+        // OR:
+        mocks: {
+          $store: store,
+        },
+      },
+    });
+    expect (wrapper.exists ()).toBe (true);
+  });
+});
