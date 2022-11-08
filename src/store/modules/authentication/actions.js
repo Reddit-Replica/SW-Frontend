b9382f8f4a3c9a4ae95fa12e1ed1287147615667
norhanreda <Norhan.ahmed01@eng-st.cu.edu.eg>
2022-11-08 12:30:02 +0200

    adding styling for more validation

--- src/store/modules/authentication/actions.js
+++ src/store/modules/authentication/actions.js
@@ -13,6 +13,61 @@ export default {
 		});
 
 		const responseData = await response.json();
+		console.log(response.status);
+		console.log(responseData);
+		if (response.ok) {
+			console.log(response);
+			console.log(responseData);
+		} else if (!response.ok) {
+			console.log(response);
+			console.log(responseData);
+			const error = new Error(responseData.error);
+			console.log(responseData.error);
+			throw error;
+		}
+	},
+	async forgetuserdhandle(context, payload) {
+		const userInfo = {
+			email: payload.email,
+		};
+		const baseurl = payload.baseurl;
+
+		const response = await fetch(baseurl + '/login/forget-username', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(userInfo),
+		});
+
+		const responseData = await response.json();
+		if (response.ok) {
+			console.log(response);
+			console.log(responseData);
+			console.log(responseData.token);
+		} else if (!response.ok) {
+			const error = new Error(responseData.error);
+			console.log(responseData.error);
+			throw error;
+		}
+	},
+	async ressethandle(context, payload) {
+		const userInfo = {
+			newPassword: payload.password,
+			verifyPassword: payload.verifyPassword,
+		};
+		const baseurl = payload.baseurl;
+		const id = payload.id;
+		const token = payload.token;
+
+		const response = await fetch(
+			baseurl + '/reset-password/' + id + '/' + token,
+			{
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(userInfo),
+			}
+		);
+
+		const responseData = await response.json();
 		if (response.ok) {
 			console.log(response);
 			console.log(responseData);
@@ -94,3 +149,15 @@ export default {
 		}
 	},
 };
+//get request example
+/*const response = await fetch('link');
+const responseData = await response.json();
+
+if (response.ok) {
+	console.log(response);
+	console.log(response.status);
+} else if (!response.ok) {
+	const error = new Error(responseData.error);
+	throw error;
+}
+*/
