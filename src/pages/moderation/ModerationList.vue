5305d0d772b553ecfb07641677ba709e64c5672c
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-11-11 17:49:12 +0200

    start creating moderators list page

---
+++ src/pages/moderation/ModerationList.vue
@@ -0,0 +1,24 @@
+<template>
+	<the-header :header-title="subredditName"></the-header>
+	<listmoderation-bar :subredditName="subredditName"></listmoderation-bar>
+	<div>List of moderators</div>
+</template>
+
+<script>
+import ListmoderationBar from '../../components/moderation/ListmoderationBar.vue';
+export default {
+	components: {
+		ListmoderationBar,
+	},
+	created() {
+		document.title = this.$store.state.subredditName;
+	},
+	computed: {
+		subredditName() {
+			return this.$store.state.subredditName;
+		},
+	},
+};
+</script>
+
+<style></style>
