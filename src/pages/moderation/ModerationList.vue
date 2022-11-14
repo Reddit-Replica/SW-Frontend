46a6895e8ccb4d0e55f34c4cab253e8d114fabc0
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-11-14 21:42:36 +0200

    start creating moderation pages

--- src/pages/moderation/ModerationList.vue
+++ src/pages/moderation/ModerationList.vue
@@ -2,77 +2,23 @@
 	<the-header :header-title="subredditName"></the-header>
 	<listmoderation-bar :subreddit-name="subredditName"></listmoderation-bar>
 	<div>
-		<div class="page-content" v-if="!moderatorByMe">
-			<h3 class="heading-3">
-				Moderators of r/{{ subredditName }}
-				<a
-					href="https://mods.reddithelp.com/hc/en-us/articles/360009381491"
-					__blank="targe"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="16"
-						height="16"
-						fill="currentColor"
-						class="bi bi-info-circle icon-info"
-						viewBox="0 0 16 16"
-					>
-						<path
-							d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-						/>
-						<path
-							d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
-						/>
-					</svg>
-				</a>
-			</h3>
-			<search-bar
-				@enter-search="(search) => enterSearch(search)"
-				:empty-input="search"
-			></search-bar>
-			<ul class="ul-items" v-if="!noItems">
-				<list-item
-					v-for="(moderator, index) in listOfModerators"
-					:key="moderator"
-					:moderator="moderator"
-					:search="search"
-					:index="index"
-				></list-item>
-			</ul>
-			<div class="no-items" v-else>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="16"
-					height="16"
-					fill="currentColor"
-					class="bi bi-search icon-search"
-					viewBox="0 0 16 16"
-					id="search"
-				>
-					<path
-						d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-					/>
-				</svg>
-				<span>No results for u/v</span>
-				<base-button
-					class="see-all-button"
-					id="see-all-button"
-					@click="seeAll()"
-					>See all</base-button
-				>
-			</div>
-		</div>
+		<unmoderator-view
+			v-if="!moderatorByMe"
+			:listOfModerators="listOfModerators"
+			:subredditName="subredditName"
+		></unmoderator-view>
 		<div v-else>
-			<leftside-bar></leftside-bar>
+			<leftside-bar :subredditName="subredditName"></leftside-bar>
 		</div>
 	</div>
 </template>
 
 <script>
 import ListmoderationBar from '../../components/moderation/ListmoderationBar.vue';
-import SearchBar from '../../components/moderation/SearchBar.vue';
-import ListItem from '../../components/moderation/ListItem.vue';
+// import SearchBar from '../../components/moderation/SearchBar.vue';
+// import ListItem from '../../components/moderation/ListItem.vue';
 import LeftsideBar from '../../components/moderation/LeftsideBar.vue';
+import UnmoderatorView from '../../components/moderation/UnmoderatorView.vue';
 export default {
 	data() {
 		return {
@@ -83,9 +29,10 @@ export default {
 	},
 	components: {
 		ListmoderationBar,
-		SearchBar,
-		ListItem,
+		// SearchBar,
+		// ListItem,
 		LeftsideBar,
+		UnmoderatorView,
 	},
 	beforeMount() {
 		document.title = this.$store.state.subredditName;
@@ -157,53 +104,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.page-content {
-	width: 95%;
-	margin: auto;
-}
-.icon-info {
-	width: 2rem;
-	height: 2rem;
-	fill: var(--color-blue-2);
-	cursor: pointer;
-}
-.heading-3 {
-	margin-left: 1rem;
-	margin-top: 5rem;
-}
-.ul-items {
-	list-style: none;
-	background-color: var(--color-white-1);
-	padding-left: 0rem;
-}
-.no-items {
-	background-color: var(--color-white-1);
-	height: 25rem;
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	font-size: 2rem;
-	font-weight: bold;
-}
-.icon-search {
-	width: 3rem;
-	height: 3rem;
-}
-.see-all-button {
-	padding: 0 1.5rem;
-	color: var(--color-blue-2);
-	background-color: var(--color-white-1);
-	font-weight: bold;
-}
-.see-all-button:hover {
-	background-color: var(--color-grey-light-8);
-}
-/* 635px */
-@media only screen and (max-width: 40em) {
-	.content {
-		width: 100%;
-	}
-}
-</style>
+<style scoped></style>
---
+++ src/pages/moderation/TheContributors.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>The contributers</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
