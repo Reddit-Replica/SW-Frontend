f5260d3d93bb58259b7d09803411d08be770e39e
MennaTalhHossamAlden <76497207+MennaTalhHossamAlden@users.noreply.github.com>
2022-11-17 17:42:38 +0200

    listing crossposts in home page

--- src/store/modules/Listing/actions.js
+++ src/store/modules/Listing/actions.js
@@ -29,7 +29,30 @@ export default {
 				votes: responseData[key].children[0].votes,
 				votingType: responseData[key].children[0].votingType,
 				saved: responseData[key].children[0].saved,
+				kind: responseData[key].children[0].kind,
+				sharePostId: responseData[key].children[0].sharePostId,
 			};
+			if (post.kind == 'post') {
+				//const QUERY_PARAMETERS = {
+				//	id: post.sharePostId,
+				//};
+				const response2 = await fetch(baseurl + '/post-details', {
+					method: 'GET',
+					headers: {
+						'Content-Type': 'application/json',
+						Authorization: `Bearer ${localStorage.getItem('userName')}`,
+					},
+					//body: JSON.stringify(QUERY_PARAMETERS),
+				});
+				const responseData2 = await response2.json();
+				if (!response2.ok) {
+					const error2 = new Error(responseData2.message || 'Failed to fetch!');
+					throw error2;
+				}
+				const sharedPostDetails = responseData2[0];
+				sharedPostDetails.id = post.sharePostId;
+				post.sharedPostDetails = sharedPostDetails;
+			}
 			posts.push(post);
 		}
 		context.commit('setPosts', posts);
