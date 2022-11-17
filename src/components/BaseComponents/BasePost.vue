6879f1a3cb2716e282a6e59d0d24eea92bfc584a
MennaTalhHossamAlden <76497207+MennaTalhHossamAlden@users.noreply.github.com>
2022-11-17 17:42:38 +0200

    listing crossposts in home page

--- src/components/BaseComponents/BasePost.vue
+++ src/components/BaseComponents/BasePost.vue
@@ -32,50 +32,7 @@
 
 		<!-- section to display post information -->
 		<div class="post-content" @click="showPostComments">
-			<div class="subreddit-info">
-				<span class="subreddit-image"
-					><img src="../../../img/user-image.jpg" alt=""
-				/></span>
-				<span class="subreddit-name">
-					<router-link
-						:to="{
-							name: 'subreddit',
-							params: { subredditName: post.subreddit },
-						}"
-						id="subreddit-router"
-						>{{ post.subreddit }}
-					</router-link>
-				</span>
-				<span>
-					. Posted by .
-					<router-link
-						:to="{ name: 'user', params: { userName: post.postedBy } }"
-						id="post-by-router"
-					>
-						{{ post.postedBy }} </router-link
-					>&nbsp;{{ post.postedAt }} ago
-				</span>
-			</div>
-			<router-link
-				:to="{
-					name: 'comments',
-					params: {
-						postName: post.title,
-						subredditName: post.subreddit,
-						postId: post.id,
-					},
-				}"
-				id="post-router"
-			>
-				<div class="post-title">
-					<h3>{{ post.title }}</h3>
-				</div>
-				<div class="post-text">
-					<p>
-						{{ post.content.substr(0, post.content.length * 0.7) }}
-					</p>
-				</div>
-			</router-link>
+			<post-content :post="post"></post-content>
 			<div class="post-services">
 				<span class="vote-services vote-box">
 					<span class="upvote" @click="upvote" id="upvote-service">
@@ -305,6 +262,7 @@
 	</div>
 </template>
 <script>
+import PostContent from '../PostContent.vue';
 export default {
 	emits: [
 		'showComments',
@@ -465,6 +423,9 @@ export default {
 			this.subMenuDisplay = false;
 		},
 	},
+	components: {
+		PostContent,
+	},
 };
 </script>
 <style scoped>
@@ -532,24 +493,6 @@ export default {
 .vote-box .vote-count.down-clicked {
 	color: var(--color-blue);
 }
-
-.post-content {
-	padding: 8px 8px 3px 5px;
-	width: 100%;
-}
-
-.post-content .post-title h3 {
-	color: black;
-	margin: 10px 0px;
-}
-
-.post-content .post-text {
-	color: black;
-	mask-image: linear-gradient(180deg, #000 60%, transparent);
-	overflow: hidden;
-	font-size: 12px;
-}
-
 .post-services .services {
 	padding: 0px;
 	margin: 0px;
@@ -583,34 +526,6 @@ export default {
 
 a {
 	text-decoration: none;
-}
-
-.post-card .subreddit-info .subreddit-image img {
-	width: 20px;
-	height: 20px;
-	border-radius: 50%;
-}
-
-.post-card .subreddit-info .subreddit-image {
-	padding-right: 5px;
-}
-
-.post-card .subreddit-info .subreddit-name a {
-	font-weight: 700;
-	color: black;
-}
-
-.post-card .subreddit-info .subreddit-name a:hover {
-	text-decoration: underline;
-}
-
-.post-card .subreddit-info span:nth-of-type(3),
-.post-card .subreddit-info span:nth-of-type(3) a {
-	color: var(--color-grey-dark-2);
-}
-
-.post-card .subreddit-info span:nth-of-type(3) a:hover {
-	text-decoration: underline;
 }
 .post-card .post-content .post-services .services .sub-menu {
 	position: absolute;
@@ -729,5 +644,9 @@ a {
 }
 #post-router-comment {
 	color: var(--color-grey-dark-2);
+}
+.post-content {
+	padding: 8px 8px 3px 5px;
+	width: 100%;
 }
 </style>
