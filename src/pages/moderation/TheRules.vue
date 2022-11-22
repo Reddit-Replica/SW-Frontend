dab89ebe7814904328f98f170fbbb4a309fc05da
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-11-22 15:51:59 +0200

    add listing rules feature in moderation

--- src/pages/moderation/TheRules.vue
+++ src/pages/moderation/TheRules.vue
@@ -4,7 +4,30 @@
 			:title="'Rules'"
 			@show-add-rule-function="showAddRuleFunction"
 		></list-bar>
-		<no-list :title="'Rules'">
+		<div class="text">
+			Rules
+			<a
+				href="https://mods.reddithelp.com/hc/en-us/articles/360009381491"
+				__blank="targe"
+			>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="16"
+					height="16"
+					fill="currentColor"
+					class="bi bi-info-circle icon-info"
+					viewBox="0 0 16 16"
+				>
+					<path
+						d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+					/>
+					<path
+						d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
+					/>
+				</svg>
+			</a>
+		</div>
+		<no-list :title="'Rules'" v-if="noRules">
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="16"
@@ -22,6 +45,20 @@
 				/>
 			</svg>
 		</no-list>
+		<div v-else>
+			<span class="rules-description">
+				These are rules that visitors must follow to participate. They can be
+				used as reasons to report or ban posts, comments, and users. Communities
+				can have a maximum of 15 rules.</span
+			>
+			<ul class="ul-items">
+				<list-rules
+					v-for="rule in listOfRules"
+					:key="rule"
+					:rule="rule"
+				></list-rules>
+			</ul>
+		</div>
 		<addrule-popup
 			v-if="showAddRule"
 			:subreddit-name="subredditName"
@@ -34,12 +71,16 @@
 import ListBar from '../../components/moderation/ListBar.vue';
 import NoList from '../../components/moderation/NoList.vue';
 import AddrulePopup from '../../components/moderation/AddrulePopup.vue';
+import ListRules from '../../components/moderation/ListRules.vue';
 export default {
-	components: { NoList, AddrulePopup, ListBar },
+	components: { NoList, AddrulePopup, ListBar, ListRules },
 	data() {
 		return {
 			showAddRule: false,
 		};
+	},
+	beforeMount() {
+		this.loadListOfRules();
 	},
 	computed: {
 		// @vuese
@@ -48,6 +89,22 @@ export default {
 		subredditName() {
 			return this.$store.state.subredditName;
 		},
+		// @vuese
+		//return list of Rules
+		// @type object
+		listOfRules() {
+			return this.$store.getters['moderation/listOfRules'];
+		},
+		// @vuese
+		//return true if there is no rules, false otherwise
+		// @type boolean
+		noRules() {
+			if (this.listOfRules.length != 0) {
+				console.log(this.listOfRules.length);
+				return false;
+			}
+			return true;
+		},
 	},
 	methods: {
 		// @vuese
@@ -55,6 +112,19 @@ export default {
 		// @arg no argument
 		showAddRuleFunction() {
 			this.showAddRule = !this.showAddRule;
+		},
+		// @vuese
+		//load Rules list from the store
+		// @arg no argument
+		async loadListOfRules() {
+			try {
+				await this.$store.dispatch('moderation/loadListOfRules', {
+					baseurl: this.$baseurl,
+					subredditName: this.subredditName,
+				});
+			} catch (error) {
+				this.error = error.message || 'Something went wrong';
+			}
 		},
 	},
 };
@@ -66,5 +136,25 @@ export default {
 	font-size: 3rem;
 	height: 3rem;
 	width: 3rem;
+}
+.text {
+	font-size: 1.8rem;
+	font-weight: 500;
+	line-height: 2.2rem;
+	color: var(--color-dark-2);
+	padding-top: 9rem;
+	margin-bottom: 1rem;
+}
+.ul-items {
+	list-style: none;
+	background-color: var(--color-white-1);
+	padding-left: 0rem;
+	margin-top: 1rem;
+}
+.rules-description {
+	font-size: 1.3rem;
+	font-weight: 400;
+	line-height: 1.8rem;
+	color: var(--color-dark-3);
 }
 </style>
