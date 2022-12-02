5d1672f77547c31d46664912e86d5e3e9e9e734b
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-12-02 14:43:28 +0200

    change tests file extention

--- tests/messages/AllinboxPage.test.js
+++ tests/messages/AllinboxPage.test.js
@@ -1,7 +1,5 @@
-import {mount} from '@vue/test-utils';
 import {shallowMount} from '@vue/test-utils';
 import AllinboxPage from '../../src/pages/messages/AllInbox.vue';
-import Vuex from 'vuex';
 import store from '../../src/store/index.js';
 
 import {describe, it, expect} from 'vitest';
@@ -19,10 +17,12 @@ describe ('AllinboxPage', () => {
     isReply: false,
     isRead: true,
   };
+  const emptyMessage = {};
 
   //--------------------------------------------------------
   //                     Rendering
   //--------------------------------------------------------
+
   it ('should renders if AllinboxPage content is correct', () => {
     const wrapper = shallowMount (AllinboxPage, {
       props: {
@@ -37,11 +37,31 @@ describe ('AllinboxPage', () => {
     });
     expect (wrapper.exists ()).toBe (true);
   });
-  
+
   //--------------------------------------------------------
-  //                     Testing no message 
+  //                     Testing no message
   //--------------------------------------------------------
-  it ('Testing no message value is correct', () => {
+
+  // it ('Testing no message value is true if there is no messages', () => {
+  //   const data = {
+  //     noMessages: true,
+  //     emptyMessage: [],
+	// 	};
+  //   const wrapper = shallowMount (AllinboxPage, {
+  //     props: {
+  //       data,
+  //     },
+  //     global: {
+  //       // OR:
+  //       mocks: {
+  //         $store: store,
+  //       },
+  //     },
+  //   });
+  //   expect (wrapper.text ()).contain ('seem to be anything here');
+  // });
+
+  it ('Testing no message value is false if there is message', () => {
     const wrapper = shallowMount (AllinboxPage, {
       props: {
         message,
