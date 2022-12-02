286bf82bebe89d8ae7c82baf0f25e74000a69822
hodagamal <hoda.ismail00@eng-st.cu.edu.eg>
2022-12-03 00:44:28 +0200

    Add documentation in community store

--- src/store/modules/communities/getters.js
+++ src/store/modules/communities/getters.js
@@ -1,3 +1,13 @@
+/**
+ * The module 'CommunityStore' object.
+ * @name CommunityGetters
+ * @type {Object}
+ * @getter {Boolean} subredditNameTaken=subredditNameTaken Returns an boolean contain check if this subreddit name is used before property.
+ * @getter {Array} categories=categories Returns an array contain recommended categories for creating a subreddit property.
+ * @getter {Object} getSubreddit=getSubreddit Returns an object contain subreddit details property.
+ * @getter {Array} getPosts=getPosts Returns an array contain posts in a subreddit property.
+ * @getter {Boolean} createdSuccessfully=createdSuccessfully Returns an boolean contain check if this subreddit created successfully property.
+ */
 export default {
 	subredditNameTaken(state) {
 		return state.subredditNameTaken;
