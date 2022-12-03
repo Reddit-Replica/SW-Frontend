9f4053af5dd2fd065d6913bb9c3b54ea0a7ec2db
hodagamal <hoda.ismail00@eng-st.cu.edu.eg>
2022-12-03 15:30:33 +0200

    Update unit testing

--- tests/Subreddits/AboutCommunity.test.js
+++ tests/Subreddits/AboutCommunity.test.js
@@ -8,6 +8,8 @@ import store from '../../src/store/index.js';
 import { describe, it, expect, vitest } from 'vitest';
 
 describe('AboutCommunityBar.vue', () => {
+
+
 	it('renders', () => {
 		const wrapper = shallowMount(AboutCommunityBar, {
 			global: {
@@ -32,34 +34,19 @@ describe('AboutCommunityBar.vue', () => {
         expect(wrapper.find('.box-options').exists()).toBe(true);
 	});
 
-  //   it('Add description', async () => {
-	// 	const wrapper = shallowMount(AboutCommunityBar, {
-	// 		global: {
-	// 			components: {},
-	// 			mocks: { $store: store },
-	// 		},
-	// 	});
+    it('Add description', async () => {
+		const wrapper = shallowMount(AboutCommunityBar, {
+			global: {
+				components: {},
+				mocks: { $store: store },
+			},
+		});
 
-	// 	//show add description text area
-	// 	await wrapper.find('#add-description-before').trigger('click');
-  //       expect(wrapper.find('#add-description-after').exists()).toBe(true);
-	// 	//hide add description text area
-  //       await wrapper.find('#cancel-description').trigger('click');
-  //       expect(wrapper.find('#add-description-after').exists()).toBe(false);
-	// });
-
-	// it('Test topics list', async () => {
-	// 	const wrapper = shallowMount(AboutCommunityBar, {
-	// 		global: {
-	// 			components: {},
-	// 			mocks: { $store: store },
-	// 		},
-	// 	});
-
-	// 	expect(wrapper.find('#add-topic').exists()).toBe(true);
-	// 	//show topics list
-	// 	await wrapper.find('#add-topic').trigger('click');
-  //       expect(wrapper.find('#topics-list').exists()).toBe(true);
-	// });
-
+		//show add description text area
+		await wrapper.find('#add-description-1').trigger('click');
+        expect(wrapper.find('#add-description-2').exists()).toBe(true);
+		//hide add description text area
+        await wrapper.find('#cancel-description').trigger('click');
+        expect(wrapper.find('#add-description-2').exists()).toBe(false);
+	});
 });
