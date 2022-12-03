915906b73f89eb26e33628add18c77c88edeea96
hodagamal <hoda.ismail00@eng-st.cu.edu.eg>
2022-12-03 18:07:47 +0200

    Fix unit tests

--- tests/Subreddits/AboutCommunity.test.js
+++ tests/Subreddits/AboutCommunity.test.js
@@ -3,12 +3,21 @@ import { shallowMount } from '@vue/test-utils';
 import AboutCommunityBar from '../../src/components/CommunityComponents/AboutCommunityBar.vue';
 import Vue from 'vue';
 import Vuex from 'vuex';
-import VueRouter from 'vue-router'
+import VueRouter from 'vue-router';
 import store from '../../src/store/index.js';
 import { describe, it, expect, vitest } from 'vitest';
 
 describe('AboutCommunityBar.vue', () => {
-
+	const subreddit = {
+		topics: ['Art', 'Anime', 'Beauty'],
+		communityTopicProp: { mainTopic: '', subtopics: [] },
+		communityDescriptionProp: '',
+		subredditName: 'Hello',
+		communityType: 'Public',
+		communityDate: 'NOV 23, 2022',
+		onlineMembersCount: 10,
+		membersCount: 20,
+	};
 
 	it('renders', () => {
 		const wrapper = shallowMount(AboutCommunityBar, {
@@ -31,10 +40,10 @@ describe('AboutCommunityBar.vue', () => {
 		expect(wrapper.find('#three-dots').exists()).toBe(true);
 		//show options list
 		await wrapper.find('#three-dots').trigger('click');
-        expect(wrapper.find('.box-options').exists()).toBe(true);
+		expect(wrapper.find('.box-options').exists()).toBe(true);
 	});
 
-    it('Add description', async () => {
+	it('Add description', async () => {
 		const wrapper = shallowMount(AboutCommunityBar, {
 			global: {
 				components: {},
@@ -44,9 +53,9 @@ describe('AboutCommunityBar.vue', () => {
 
 		//show add description text area
 		await wrapper.find('#add-description-1').trigger('click');
-        expect(wrapper.find('#add-description-2').exists()).toBe(true);
+		expect(wrapper.find('#add-description-2').exists()).toBe(true);
 		//hide add description text area
-        await wrapper.find('#cancel-description').trigger('click');
-        expect(wrapper.find('#add-description-2').exists()).toBe(false);
+		await wrapper.find('#cancel-description').trigger('click');
+		expect(wrapper.find('#add-description-2').exists()).toBe(false);
 	});
 });
