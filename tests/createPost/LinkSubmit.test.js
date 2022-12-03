e1a6a3100943ae675072cea95e432f7364a58560
norhanreda <Norhan.ahmed01@eng-st.cu.edu.eg>
2022-12-03 18:52:53 +0200

    adding some test files

---
+++ tests/createPost/LinkSubmit.test.js
@@ -0,0 +1,29 @@
+import { describe, expect, it, vitest } from 'vitest';
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import  LinkSubmit from '../../src/components/SubmitComponents/LinkSubmit.vue'
+import Vuex from 'vuex';
+import store from '../../src/store/index.js';
+import mockservice from '../../../mockservice.txt';
+
+describe('Submit footer rendring', () => {
+
+	//it-> test block
+	it('Render component exist', async () => {
+		//our component
+		const wrapper = shallowMount(LinkSubmit, {
+			global: {
+				components: {},
+				mocks: {},
+			},
+		});
+		expect(wrapper.exists()).toBe(true);
+	});
+
+
+
+
+
+
+
+});
+
