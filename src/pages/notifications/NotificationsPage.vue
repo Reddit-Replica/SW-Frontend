8231e535a6c8808b02c6230ad227d4f6b577f285
hodagamal <hoda.ismail00@eng-st.cu.edu.eg>
2022-12-06 10:54:47 +0200

    Add notifications top bar

---
+++ src/pages/notifications/NotificationsPage.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<the-header :header-title="'u/asmaaadel0'"></the-header>
+		<div>
+			<notifications-top></notifications-top>
+		</div>
+	</div>
+</template>
+
+<script>
+import NotificationsTop from '../../components/NotificationsComponents/NotificationsTop.vue';
+
+export default {
+	components: {
+		NotificationsTop,
+	},
+};
+</script>
