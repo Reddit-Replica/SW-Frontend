686dae13ca75a8a89698389cda5c894dfe52e412
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2022-12-09 21:51:40 +0200

    adding strore for setting

---
+++ src/store/modules/settings/index.js
@@ -0,0 +1,12 @@
+import mutations from './mutations.js';
+import actions from './actions.js';
+import getters from './getters.js';
+
+export default {
+	state() {
+		return {};
+	},
+	mutations,
+	actions,
+	getters,
+};
