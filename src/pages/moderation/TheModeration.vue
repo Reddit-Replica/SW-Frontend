38d8338b4ac771f1811f4a48a0c939ef213ab178
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-12-09 15:38:11 +0200

    handle if condition

--- src/pages/moderation/TheModeration.vue
+++ src/pages/moderation/TheModeration.vue
@@ -7,7 +7,7 @@
 	<div>
 		<!-- ///////////change to !moderatorByMe/////////// -->
 		<unmoderator-view
-			v-if="moderatorByMe"
+			v-if="!moderatorByMe"
 			:list-of-moderators="listOfModerators"
 			:subreddit-name="subredditName"
 		></unmoderator-view>
