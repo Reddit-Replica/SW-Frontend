249a4ce7f399a74f6e70a924ee78baa0d922f5a6
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2022-12-12 17:49:41 +0200

    integrate email setting with back end

--- src/store/modules/settings/actions.js
+++ src/store/modules/settings/actions.js
@@ -1,5 +1,33 @@
 export default {
-	async changeEmailsetting(context, payload) {
+	async changenewFollowerEmail(context, payload) {
+		const setting = {
+			newFollowerEmail: payload.newFollowerEmail,
+		};
+
+		const baseurl = payload.baseurl;
+		const response = await fetch(baseurl + '/account-settings', {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+			},
+			body: JSON.stringify(setting),
+		});
+		const responseData = await response.json();
+		if (response.status == 200) {
+			console.log(response);
+		} else if (response.status == 401) {
+			const error = new Error(responseData.error);
+			console.log(error);
+			throw error;
+		} else {
+			const error = new Error('server error');
+			console.log(error);
+			throw error;
+		}
+		return response.status;
+	},
+	async changeunsubscribeFromEmails(context, payload) {
 		const setting = {
 			unsubscribeFromEmails: payload.unsubscribeFromEmails,
 		};
@@ -14,7 +42,7 @@ export default {
 			body: JSON.stringify(setting),
 		});
 		const responseData = await response.json();
-		if (response.status == 201) {
+		if (response.status == 200) {
 			console.log(response);
 		} else if (response.status == 401) {
 			const error = new Error(responseData.error);
