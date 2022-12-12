2570b2195e98ae8cb124ab898b08b5353058de6a
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2022-12-12 09:06:07 +0200

    adding setters and getters for email  setting

--- src/store/modules/settings/actions.js
+++ src/store/modules/settings/actions.js
@@ -1,1 +1,30 @@
-export default {};
+export default {
+	async changeEmailsetting(context, payload) {
+		const setting = {
+			unsubscribeFromEmails: payload.unsubscribeFromEmails,
+		};
+
+		const baseurl = payload.baseurl;
+		const response = await fetch(baseurl + '/account-settings', {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+			},
+			body: JSON.stringify(setting),
+		});
+		const responseData = await response.json();
+		if (response.status == 201) {
+			console.log(response);
+		} else if (response.status == 401) {
+			const error = new Error(responseData.error);
+			console.log(error);
+			throw error;
+		} else {
+			const error = new Error('server error');
+			console.log(error);
+			throw error;
+		}
+		return response.status;
+	},
+};
