79a65bbed5661a399d815ab6cc6b4ae9c9a341ba
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-12-12 16:46:05 +0200

    fix error in unit testing

--- tests/Subreddits/GrowCommunity.test.js
+++ tests/Subreddits/GrowCommunity.test.js
@@ -1,37 +1,81 @@
-import { mount } from '@vue/test-utils';
-import { shallowMount } from '@vue/test-utils';
-import GrowCommunity from '../../src/components/CommunityComponents/GrowCommunity.vue';
+import {mount} from '@vue/test-utils';
+import {shallowMount} from '@vue/test-utils';
+import GrowCommunity
+  from '../../src/components/CommunityComponents/GrowCommunity.vue';
 import Vue from 'vue';
 import Vuex from 'vuex';
 import VueRouter from 'vue-router';
-import store from '../../src/store/index.js';
-import { describe, it, expect, vitest } from 'vitest';
+// import store from '../../src/store/index.js';
+import {describe, it, expect, vitest} from 'vitest';
 
-describe('GrowCommunity.vue', () => {
-	it('renders', () => {
-		const wrapper = shallowMount(GrowCommunity, {
-			global: {
-				components: {},
-				mocks: { $store: store },
-			},
-		});
-		expect(wrapper.exists()).toBe(true);
-	});
+describe ('GrowCommunity.vue', () => {
+  let store = new Vuex.Store ({
+    modules: {
+      userModule: {
+        namespaced: true,
+        state: {
+          userData: {
+            displayName: '',
+            about: '',
+            banner: '',
+            picture: '',
+            karma: 0,
+            cakeDate: '2019-08-24',
+            socialLinks: [
+              {
+                type: 'Reddit',
+                displayText: 'medo',
+                link: '11',
+              },
+            ],
+            nsfw: true,
+            followed: true,
+            blocked: true,
+            moderatorOf: [
+              {
+                subredditName: '',
+                numOfMembers: 0,
+                nsfw: true,
+              },
+            ],
+          },
+          socialLinkItems: [
+            {
+              text: 'Custom URL',
+              imgSrc: 'img',
+              alt: 'custom url',
+              type: 'link' /* link or username */,
+              baseurl: '',
+            },
+          ],
+        },
+      },
+    },
+  });
+  it ('renders', () => {
+    const wrapper = shallowMount (GrowCommunity, {
+      global: {
+        components: {},
+        mocks: {$store: store},
+      },
+    });
+    expect (wrapper.exists ()).toBe (true);
+  });
 
-	it('Test show and hide', async () => {
-		const wrapper = shallowMount(GrowCommunity, {
-			global: {
-				components: {},
-				mocks: { $store: store },
-			},
-		});
+  it ('Test show and hide', async () => {
+    const wrapper = shallowMount (GrowCommunity, {
+      global: {
+        components: {},
+        mocks: {$store: store},
+      },
+    });
 
-		expect(wrapper.find('.grow-body').exists()).toBe(true);
-		//hide grow your community part
-		await wrapper.find('#show-up-grow').trigger('click');
-		expect(wrapper.find('.grow-body').exists()).toBe(false);
-		//show  grow your community part
-		await wrapper.find('#show-down-grow').trigger('click');
-		expect(wrapper.find('.grow-body').exists()).toBe(true);
-	});
+    expect (wrapper.find ('.grow-body').exists ()).toBe (true);
+    //hide grow your community part
+    await wrapper.find ('#show-up-grow').trigger ('click');
+    expect (wrapper.find ('.grow-body').exists ()).toBe (false);
+    //show  grow your community part
+    await wrapper.find ('#show-down-grow').trigger ('click');
+    expect (wrapper.find ('.grow-body').exists ()).toBe (true);
+  });
 });
--- tests/createPost/SubmitPost.test.js
+++ tests/createPost/SubmitPost.test.js
@@ -1,57 +1,91 @@
-import { describe, expect, it, vitest } from 'vitest';
-import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
-import SubmitPage from '../../src/pages/submit/SubmitPage.vue'
+import {describe, expect, it, vitest} from 'vitest';
+import {shallowMount, mount, createLocalVue} from '@vue/test-utils';
+import SubmitPage from '../../src/pages/submit/SubmitPage.vue';
 import Vuex from 'vuex';
-import store from '../../src/store/index.js';
+// import store from '../../src/store/index.js';
 import mockservice from '../../../mockservice.txt';
 
-describe('SubmitPage rendring', () => {
+describe ('SubmitPage rendring', () => {
+  let store = new Vuex.Store ({
+    modules: {
+      userModule: {
+        namespaced: true,
+        state: {
+          userData: {
+            displayName: '',
+            about: '',
+            banner: '',
+            picture: '',
+            karma: 0,
+            cakeDate: '2019-08-24',
+            socialLinks: [
+              {
+                type: 'Reddit',
+                displayText: 'medo',
+                link: '11',
+              },
+            ],
+            nsfw: true,
+            followed: true,
+            blocked: true,
+            moderatorOf: [
+              {
+                subredditName: '',
+                numOfMembers: 0,
+                nsfw: true,
+              },
+            ],
+          },
+          socialLinkItems: [
+            {
+              text: 'Custom URL',
+              imgSrc: 'img',
+              alt: 'custom url',
+              type: 'link' /* link or username */,
+              baseurl: '',
+            },
+          ],
+        },
+      },
+    },
+  });
 
-    const categories = ['Gaming', 'Sports', 'Memes', 'News'];
+  const categories = ['Gaming', 'Sports', 'Memes', 'News'];
 
-	const communityAction = {
-		createPost: vi.fn(),
-
-	};
-	const communityMutation = {
-		setTitle: vi.fn(),
-		setNsfw: vi.fn(),
-		setSpoiler: vi.fn(),
-		setFlairId: vi.fn(),
-		setsendReplies: vi.fn(),
-        setContent: vi.fn(),
-        setallSubreddits: vi.fn(),
-        setSubreddit: vi.fn(),
-        setinSubreddit: vi.fn(),
-	};
-	const communityGetters = {
-		getTitle: vi.fn(),
-		getNsfw: vi.fn(),
-		getSpoiler: vi.fn(),
-		getFlairId: vi.fn(),
-		getsendReplies: vi.fn(),
-        getContent: vi.fn(),
-        getallSubreddits: vi.fn(),
-        getSubreddit: vi.fn(),
-        getinSubreddit: vi.fn(),
-	};
-	//it-> test block
-	it('Render component exist', async () => {
-		//our component
-		const wrapper = shallowMount(SubmitPage, {
-			global: {
-				components: {},
-				mocks: {},
-			},
-		});
-		expect(wrapper.exists()).toBe(true);
-	});
-
-
-
-
-
-
-
+  const communityAction = {
+    createPost: vi.fn (),
+  };
+  const communityMutation = {
+    setTitle: vi.fn (),
+    setNsfw: vi.fn (),
+    setSpoiler: vi.fn (),
+    setFlairId: vi.fn (),
+    setsendReplies: vi.fn (),
+    setContent: vi.fn (),
+    setallSubreddits: vi.fn (),
+    setSubreddit: vi.fn (),
+    setinSubreddit: vi.fn (),
+  };
+  const communityGetters = {
+    getTitle: vi.fn (),
+    getNsfw: vi.fn (),
+    getSpoiler: vi.fn (),
+    getFlairId: vi.fn (),
+    getsendReplies: vi.fn (),
+    getContent: vi.fn (),
+    getallSubreddits: vi.fn (),
+    getSubreddit: vi.fn (),
+    getinSubreddit: vi.fn (),
+  };
+  //it-> test block
+  it ('Render component exist', async () => {
+    //our component
+    const wrapper = shallowMount (SubmitPage, {
+      global: {
+        components: {},
+        mocks: {$store: store},
+      },
+    });
+    expect (wrapper.exists ()).toBe (true);
+  });
 });
-
