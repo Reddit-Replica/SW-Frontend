4a8867fbe40925fbbecc2f910169f79d4b9b5c27
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-12-12 22:40:20 +0200

    add approve popup

--- src/components/moderation/ApprovedItem.vue
+++ src/components/moderation/ApprovedItem.vue
@@ -1,33 +1,71 @@
 <template>
-	<li class="item" v-if="show">
-		<div class="image" id="moderator">
-			<img
-				v-if="!approve.avatar"
-				src="../../../img/default_inbox_avatar.png"
-				alt="img"
-				class="img"
-				:id="'moderator-img-' + index"
-			/>
-			<img
-				v-else
-				:src="$baseurl + '/' + approve.avatar"
-				alt="img"
-				class="img"
-				:id="'moderator-img-' + index"
-			/>
-			<h5 class="heading-5" :id="'moderator-name-' + index">
-				{{ approve.username }}
-			</h5>
-		</div>
-		<div class="time">
-			<span>{{ handleTime }}</span>
-		</div>
-		<div class="remove">
-			<base-button class="button-remove" :id="'button-remove-' + index"
-				>Remove</base-button
+	<div>
+		<li class="item" v-if="show">
+			<div class="image" id="moderator">
+				<img
+					v-if="!approve.avatar"
+					src="../../../img/default_inbox_avatar.png"
+					alt="img"
+					class="img"
+					:id="'moderator-img-' + index"
+				/>
+				<img
+					v-else
+					:src="$baseurl + '/' + approve.avatar"
+					alt="img"
+					class="img"
+					:id="'moderator-img-' + index"
+				/>
+				<h5 class="heading-5" :id="'moderator-name-' + index">
+					{{ approve.username }}
+				</h5>
+			</div>
+			<div class="time">
+				<span>{{ handleTime }}</span>
+			</div>
+			<div class="remove">
+				<base-button
+					class="button-remove"
+					:id="'button-remove-' + index"
+					@click="showApproveFunction()"
+					>Remove</base-button
+				>
+			</div>
+		</li>
+		<div id="sure-popup-form">
+			<base-dialog
+				:show="showApprove"
+				@close="showApproveFunction()"
+				title="Confirm"
 			>
+				<div class="rule-dialog flex-column">
+					<div class="rule-box-p flex-column">
+						<p class="sure-text">
+							Are you sure you want to remove {{ approve.username }} as an
+							approved user?
+						</p>
+					</div>
+					<div class="rule-box box-buttons">
+						<base-button
+							@click="showApproveFunction()"
+							class="button-white"
+							id="cancel-button"
+							>Cancel</base-button
+						>
+						<base-button
+							@click="removeFunction()"
+							class="button-blue"
+							id="delete-button"
+							>Remove</base-button
+						>
+					</div>
+					<div class="no-messages" v-if="errorResponse">
+						{{ errorResponse }}
+					</div>
+				</div>
+			</base-dialog>
 		</div>
-	</li>
+	</div>
 </template>
 
 <script>
@@ -67,6 +105,8 @@ export default {
 	data() {
 		return {
 			handleTime: '',
+			showApprove: false,
+			errorResponse: null,
 		};
 	},
 	computed: {
@@ -116,6 +156,16 @@ export default {
 			}
 			this.handleTime = returnValue;
 		},
+		// @vuese
+		//used to show sure approve popup
+		// @arg no argument
+		showApproveFunction() {
+			this.showApprove = !this.showApprove;
+		},
+		// @vuese
+		//used to handle remove request
+		// @arg no argument
+		removeFunction() {},
 	},
 };
 </script>
@@ -179,6 +229,92 @@ export default {
 }
 .heading-5 {
 	color: var(--color-dark-1);
+}
+.rule-dialog {
+	max-height: 100%;
+	max-width: 53.8rem;
+	min-width: 41rem;
+}
+.sure-text {
+	line-height: 2.2rem;
+	margin: 1rem 0;
+	white-space: pre-wrap;
+	font-family: Noto Sans, Arial, sans-serif;
+	font-weight: 400;
+	color: var(--color-dark-3);
+	font-size: 1.4rem;
+	line-height: 2.1rem;
+	display: block;
+}
+.rule-box-p {
+	align-items: flex-start;
+	margin-top: 1.2rem;
+	margin-bottom: 3rem;
+}
+.box {
+	margin-top: 1rem;
+	width: 15rem;
+	background-color: var(--color-dark-1);
+	color: var(--color-white-1);
+	font-size: 0.9rem;
+	padding: 1rem;
+	position: absolute;
+	border-radius: 0.4rem;
+	text-align: center;
+	pointer-events: none;
+	transform: translateX(-50%);
+	z-index: 100;
+	right: -16rem;
+}
+.box-buttons {
+	background-color: var(--color-grey-light-2);
+	padding: 16px;
+	margin: 16px -16px -16px;
+	display: flex;
+	justify-content: flex-end;
+	border-bottom-right-radius: 4px;
+	box-sizing: border-box;
+}
+button {
+	min-height: 32px;
+	min-width: 32px;
+	padding: 4px 16px;
+	margin-left: 8px;
+	font-size: 14px;
+	font-weight: 700;
+}
+.button-blue:hover {
+	background-color: rgb(248, 89, 89);
+	border: 1px solid rgb(248, 89, 89);
+	padding: 0.4rem 1.6rem;
+}
+.button-white {
+	border: 1px solid var(--color-blue-2);
+	color: var(--color-blue-2);
+	padding: 0.4rem 1.6rem;
+}
+.button-white:hover {
+	border: 1px solid var(--color-grey-dark-8);
+	color: var(--color-grey-dark-8);
+	padding: 0.4rem 1.6rem;
+}
+.button-blue {
+	background-color: var(--color-blue-2);
+	border: 1px solid var(--color-blue-2);
+	color: var(--color-white-1);
+	padding: 0.4rem 1.6rem;
+}
+.disabled {
+	cursor: not-allowed;
+	filter: grayscale(1);
+	border: none;
+	color: rgba(255, 255, 255, 0.5);
+	fill: rgba(255, 255, 255, 0.5);
+	background-color: var(--color-grey-light-5);
+}
+.no-messages {
+	margin-top: 2rem;
+	padding: 1rem;
 }
 /* 340px */
 @media only screen and (max-width: 21.25em) {
