6d390f24ab408428778c73d866aa9496516e8d73
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2022-12-13 12:10:02 +0200

    adding community setting action

--- src/store/modules/settings/actions.js
+++ src/store/modules/settings/actions.js
@@ -55,4 +55,46 @@ export default {
 		}
 		return response.status;
 	},
+	//////////////////// this part for moderation setting //////////////////////////
+
+	async communitySettings(context, payload) {
+		const setting = {
+			communityName: payload.communityName,
+			mainTopic: payload.mainTopic,
+			sunTopics: payload.sunTopics,
+			communityDescription: payload.communityDescription,
+			sendWelcomeMessage: payload.sendWelcomeMessage,
+			welcomeMessage: payload.welcomeMessage,
+			language: payload.language,
+			Region: payload.Region,
+			Type: payload.Type,
+			NSFW: payload.NSFW,
+			acceptingRequestsToJoin: payload.acceptingRequestsToJoin,
+			acceptingRequestsToPost: payload.acceptingRequestsToPost,
+			approvedUsersHaveTheAbilityTo: payload.approvedUsersHaveTheAbilityTo,
+		};
+
+		const baseurl = payload.baseurl;
+		const response = await fetch(baseurl + '/r/{subreddit}/about/edit', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+			},
+			body: JSON.stringify(setting),
+		});
+		const responseData = await response.json();
+		if (response.status == 200) {
+			console.log(response);
+		} else if (response.status == 401) {
+			const error = new Error(responseData.error);
+			console.log(error);
+			throw error;
+		} else {
+			const error = new Error('server error');
+			console.log(error);
+			throw error;
+		}
+		return response.status;
+	},
 };
