4e29d7433d172737e41937f794e0bda1d60a7ce3
hodagamal <hoda.ismail00@eng-st.cu.edu.eg>
2022-12-13 21:27:33 +0200

    Add lood community in specific category in subredditleaderboard

--- src/store/modules/topCommunities/actions.js
+++ src/store/modules/topCommunities/actions.js
@@ -1,5 +1,6 @@
 export default {
 	async getAllCommunities(context, payload) {
+		console.log(payload);
 		const baseurl = payload.baseurl;
 
 		const response = await fetch(baseurl + '/subreddits/leaderboard', {
@@ -11,6 +12,7 @@ export default {
 		});
 
 		const responseData = await response.json();
+		console.log(responseData);
 
 		if (!response.ok) {
 			const error = new Error(
@@ -22,10 +24,47 @@ export default {
 		if (response.status == 200) {
 			context.commit('setAllCommunities', responseData['children']);
 		} else if (response.status == 401) {
-			const error = new Error(responseData.error || 'Bad Request');
+			const error = new Error(responseData.error || 'Unauthorized');
 			throw error;
 		} else if (response.status == 404) {
-			const error = new Error(responseData.error || 'Bad Request');
+			const error = new Error(responseData.error || 'Not Found');
+			throw error;
+		} else if (response.status == 500) {
+			const error = new Error(responseData.error || 'Server Error');
+			throw error;
+		}
+	},
+	async getCategoryCommunities(context, payload) {
+		const baseurl = payload.baseurl;
+
+		const response = await fetch(
+			baseurl + `/subreddits/leaderboard/${payload.category}`,
+			{
+				method: 'GET',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: 'Bearer ' + payload.token,
+				},
+			}
+		);
+
+		const responseData = await response.json();
+		console.log(responseData);
+
+		if (!response.ok) {
+			const error = new Error(
+				responseData.message || 'Failed to send request.'
+			);
+			throw error;
+		}
+
+		if (response.status == 200) {
+			context.commit('setCategoryCommunities', responseData['children']);
+		} else if (response.status == 401) {
+			const error = new Error(responseData.error || 'Unauthorized');
+			throw error;
+		} else if (response.status == 404) {
+			const error = new Error(responseData.error || 'Not Found');
 			throw error;
 		} else if (response.status == 500) {
 			const error = new Error(responseData.error || 'Server Error');
