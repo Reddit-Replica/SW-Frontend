bf5ba9ee72ee89c0f79cc176d24da814a6b9a41a
unknown <kareemsaqer1@gmail.com>
2022-12-15 15:37:04 +0200

    Unmoderated Post

--- src/pages/moderation/TheUnmoderated.vue
+++ src/pages/moderation/TheUnmoderated.vue
@@ -1,23 +1,42 @@
 <template>
 	<div>
 		<queue-bar :title="'Unmoderated'"></queue-bar>
-		<clean-queue></clean-queue>
+		<div v-if="UnmoderatedPosts && UnmoderatedPosts.length == 0">
+			<clean-queue></clean-queue>
+		</div>
+		<div v-else>
+			<div v-for="value in UnmoderatedPosts" :key="value.id">
+				<new-base-post
+					:spam="UnmoderatedPosts"
+					:index="value.id"
+				></new-base-post>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 import CleanQueue from '../../components/moderation/CleanQueue.vue';
 import QueueBar from '../../components/moderation/QueueBar.vue';
+import NewBasePost from '../../components/moderation/NewBasePost.vue';
 export default {
-	components: { CleanQueue, QueueBar },
+	components: { CleanQueue, QueueBar, NewBasePost },
 	beforeMount() {
 		this.Unmoderate();
 	},
 	computed: {
 		// @vuese
-		//return Unmoderated post
+		//return subreddit name
 		// @type string
+		subredditName() {
+			// return this.$store.state.subredditName;
+			return this.$route.params.subredditName;
+		},
+		// @vuese
+		//return Unmoderated post
+		// @type Object
 		UnmoderatedPosts() {
+			console.log(this.$store.getters['moderation/Unmoderated']);
 			return this.$store.getters['moderation/Unmoderated'];
 		},
 	},
