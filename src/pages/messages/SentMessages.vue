315395fa377262a946db4378c55a3eb87e94d2e7
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2022-12-18 00:54:02 +0200

    update after, before in messages

--- src/pages/messages/SentMessages.vue
+++ src/pages/messages/SentMessages.vue
@@ -16,6 +16,30 @@
 				:index="index"
 			></sent-messages>
 		</div>
+		<button
+			class="load-more"
+			v-if="!loading && after"
+			@click.prevent="loadSentMessages('after')"
+		>
+			load more
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="16"
+				height="16"
+				fill="currentColor"
+				class="bi bi-chevron-double-down"
+				viewBox="0 0 16 16"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+				/>
+				<path
+					fill-rule="evenodd"
+					d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+				/>
+			</svg>
+		</button>
 		<div class="no-messages" v-if="errorResponse">{{ errorResponse }}</div>
 	</div>
 </template>
@@ -44,14 +68,22 @@ export default {
 			count: 0,
 			errorResponse: null,
 			loading: false,
+			sentMessages: [],
 		};
 	},
 	computed: {
+		// // @vuese
+		// //return all messages sent or recieved
+		// // @type object
+		// sentMessages() {
+		// 	return this.$store.getters['messages/sentMessages'];
+		// },
+
 		// @vuese
-		//return all messages sent or recieved
-		// @type object
-		sentMessages() {
-			return this.$store.getters['messages/sentMessages'];
+		//return if there is messages after
+		// @type string
+		after() {
+			return this.$store.getters['messages/after'];
 		},
 	},
 	watch: {
@@ -66,17 +98,40 @@ export default {
 		// @vuese
 		//load messages from the store
 		// @arg no argument
-		async loadSentMessages() {
+		async loadSentMessages(title) {
+			this.loading = true;
+			let afterMod = '';
+			if (title == 'after') {
+				afterMod = this.after;
+			}
 			try {
 				await this.$store.dispatch('messages/loadSentMessages', {
 					baseurl: this.$baseurl,
+					afterMod: afterMod,
 				});
 			} catch (error) {
 				this.errorResponse = error.message || 'Something went wrong';
 			}
+			this.sentMessages = this.sentMessages.concat(
+				this.$store.getters['messages/sentMessages']
+			);
+			this.loading = false;
 		},
 	},
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.load-more {
+	font-size: 1.5rem;
+	font-weight: bold;
+	margin: 2rem;
+	cursor: pointer;
+	border: 1px solid #c0c0c0;
+	padding: 0.5rem;
+	border-radius: 2rem;
+}
+.load-more:hover {
+	background-color: aliceblue;
+}
+</style>
