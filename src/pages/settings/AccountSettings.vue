2c52bd4838a9697eb7dd779f3b938466f690e954
norhanreda <norhan.ahmed01@eng-st.cu.edu.eg>
2022-12-19 21:09:26 +0200

    comment buttons in account setting

--- src/pages/settings/AccountSettings.vue
+++ src/pages/settings/AccountSettings.vue
@@ -439,7 +439,7 @@
 					/>
 				</div>
 			</div>
-			<h3 class="h3-main-title">BETA TESTS</h3>
+			<!-- <h3 class="h3-main-title">BETA TESTS</h3>
 			<div class="section">
 				<div>
 					<h3 class="h3-title">Opt into beta tests</h3>
@@ -461,7 +461,7 @@
 				<div>
 					<switch-button id="sb-1" />
 				</div>
-			</div>
+			</div> -->
 			<h3 class="h3-main-title">DELETE ACCOUNT</h3>
 			<div class="section">
 				<div id="delete-account" @click="deletingAccount">
