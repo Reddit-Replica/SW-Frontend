043294da190f48a40edffa47fc1cea22baba97a3
unknown <kareemsaqer1@gmail.com>
2022-12-20 20:10:16 +0200

    delete unwanted packages

--- src/main.js
+++ src/main.js
@@ -60,15 +60,15 @@ function initSdk(options) {
 		resolve(window.FB)
 	  }; // eslint-disable-line
 		/* eslint-disable */
-	  // prettier-ignore
-	  (function (d, s, id) {
+		// prettier-ignore
+		(function (d, s, id) {
 		const fjs = d.getElementsByTagName(s)[0]
 		if (d.getElementById(id)) { return; }
 		const js = d.createElement(s); js.id = id
 		js.src = '//connect.facebook.net/en_US/sdk.js'
 		fjs.parentNode.insertBefore(js, fjs)
 	  }(document, 'script', 'facebook-jssdk'))
-	  /* eslint-enable */
+		/* eslint-enable */
 	});
 }
 
